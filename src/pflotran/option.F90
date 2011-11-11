f89f46a13d1501f095420a85679d504b0e55ab72
Gautam Bisht <gb9@ornl.gov>
2011-11-11 07:54:14 -0500

    Removed write statements

--- src/pflotran/option.F90
+++ src/pflotran/option.F90
@@ -293,7 +293,6 @@ function OptionCreate()
   ! simulations or initialization for every realization (e.g. within multiple 
   ! stochastic simulations).  This is done in OptionInitAll() and
   ! OptionInitRealization()
-  write(iulog,*), 'call OptionInitAll()'
   call OptionInitAll(option)
   OptionCreate => option
   
@@ -379,7 +378,6 @@ subroutine OptionInitAll(option)
   option%num_threads = 1
   option%test_res = 0 
  
-  write(iulog,*), 'call OptionInitRealization()'
   call OptionInitRealization(option)
 
 end subroutine OptionInitAll
