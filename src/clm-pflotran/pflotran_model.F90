e83750b38f3f0997f285b7f0737887eb71f2e6e1
Tang, Guoping <tangg@ornl.gov>
2013-09-10 21:57:11 -0400

    add n uptake again (something wrong with the merge)

--- src/clm-pflotran/pflotran_model.F90
+++ src/clm-pflotran/pflotran_model.F90
@@ -2448,7 +2448,7 @@ end subroutine pflotranModelSetICs
     PetscInt           :: local_id
 
     PetscScalar, pointer :: rate_pf_loc(:)   !
-!    PetscScalar, pointer :: rate_plantnuptake_pf_loc(:)   !
+    PetscScalar, pointer :: rate_plantnuptake_pf_loc(:)   !
 
 !    PetscReal, pointer :: v_p(:)
     PetscReal, pointer :: volume_p(:)
@@ -2525,7 +2525,9 @@ end subroutine pflotranModelSetICs
 
 !   get cell volume as mass transfer rate unit is mol/s
     call VecGetArrayReadF90(field%volume,volume_p,ierr)
+    call VecGetArrayReadF90(clm_pf_idata%rate_plantnuptake_pf, rate_plantnuptake_pf_loc, ierr)
 
+!    write(*, *) (rate_plantnuptake_pf_loc(local_id), local_id=1,grid%ngmax)
     if (associated(realization%rt_mass_transfer_list)) then
        cur_mass_transfer => realization%rt_mass_transfer_list
        do
@@ -2536,21 +2538,21 @@ end subroutine pflotranModelSetICs
 
          select case (cur_mass_transfer%idof)
             case(7)
-             call VecGetArrayF90(clm_pf_idata%rate_lit1c_pf, rate_pf_loc, ierr)
+             call VecGetArrayReadF90(clm_pf_idata%rate_lit1c_pf, rate_pf_loc, ierr)
             case(8)
-             call VecGetArrayF90(clm_pf_idata%rate_lit2c_pf, rate_pf_loc, ierr)
+             call VecGetArrayReadF90(clm_pf_idata%rate_lit2c_pf, rate_pf_loc, ierr)
             case(9)
-             call VecGetArrayF90(clm_pf_idata%rate_lit3c_pf, rate_pf_loc, ierr)
+             call VecGetArrayReadF90(clm_pf_idata%rate_lit3c_pf, rate_pf_loc, ierr)
             case(10)
-             call VecGetArrayF90(clm_pf_idata%rate_lit1n_pf, rate_pf_loc, ierr)
+             call VecGetArrayReadF90(clm_pf_idata%rate_lit1n_pf, rate_pf_loc, ierr)
             case(11)
-             call VecGetArrayF90(clm_pf_idata%rate_lit2n_pf, rate_pf_loc, ierr)
+             call VecGetArrayReadF90(clm_pf_idata%rate_lit2n_pf, rate_pf_loc, ierr)
             case(12)
-             call VecGetArrayF90(clm_pf_idata%rate_lit3n_pf, rate_pf_loc, ierr)
+             call VecGetArrayReadF90(clm_pf_idata%rate_lit3n_pf, rate_pf_loc, ierr)
             case(1)
-             call VecGetArrayF90(clm_pf_idata%rate_minn_pf, rate_pf_loc, ierr)
+             call VecGetArrayReadF90(clm_pf_idata%rate_minn_pf, rate_pf_loc, ierr)
             case(13)
-             call VecGetArrayF90(clm_pf_idata%rate_plantnuptake_pf, rate_pf_loc, ierr)
+             call VecGetArrayReadF90(clm_pf_idata%rate_plantnuptake_pf, rate_pf_loc, ierr)
             case default
                     pflotran_model%option%io_buffer = 'Error: set PFLOTRAN BGC rates using CLM'
                     call printErrMsg(pflotran_model%option)
@@ -2563,7 +2565,15 @@ end subroutine pflotranModelSetICs
             !v_p(local_id) = rate_pf_loc(local_id)*volume_p(local_id)  ! mol/m3s * m3
             cur_mass_transfer%dataset%rarray(local_id) = &
                         rate_pf_loc(local_id)*volume_p(local_id)  ! mol/m3s * m3
-
+            if(cur_mass_transfer%idof .eq. 1) then
+               cur_mass_transfer%dataset%rarray(local_id) = &
+                        cur_mass_transfer%dataset%rarray(local_id) + &
+                        rate_plantnuptake_pf_loc(local_id)*volume_p(local_id)  ! mol/m3s * m3
+            endif 
+            if(cur_mass_transfer%idof .eq. 13) then
+               cur_mass_transfer%dataset%rarray(local_id) = &
+                        cur_mass_transfer%dataset%rarray(local_id) * (-1.0d0)
+            endif 
          enddo
          ! F.-M. Yuan: the following call IS useless, because 'MasstransferUpdate' will always update 'v_p' and 'vec'
          ! (in fact, it will fail the model, throwing out segmentation fault)
@@ -2596,6 +2606,7 @@ end subroutine pflotranModelSetICs
     endif
 
     call VecRestoreArrayReadF90(field%volume,volume_p,ierr)
+    call VecRestoreArrayReadF90(clm_pf_idata%rate_plantnuptake_pf, rate_plantnuptake_pf_loc, ierr)
 
   end subroutine pflotranModelSetBGCRates
 
