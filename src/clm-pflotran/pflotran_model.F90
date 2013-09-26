372994fcefae128c3b54495f68f9a0e499cae6ef
Ben Andre <none@none>
2013-09-25 19:42:46 -0700

    clm-pflotran: reenable checkpoint/restart of the clm-pflotran coupled model.
    There are two outstanding issues:
    
    1) Something has changed with waypoints, so the new waypoints that clm
    is adding to pflotran are not always picked up correctly. When a clm
    simulation starts after the final time specified in the pflotran input
    file, pflotran will segfault in timestepper_base.F90 inside
    TimestepperBaseSetTargetTime(). This is not strictly related to
    checkpoint/restart, but will probably come up most frequently when
    doing multiple clm restarts. The simple work around is to ensure the
    final time in the pflotran input file is larger than the clm stop
    time.
    
    2) pflotran richards mode restarts should be bit for bit, but TH mode
    is not currently bit for bit. This will take some investigation to
    determine what's wrong. restart of TH modes should still be usable for
    development, debugging and experimental simulations that don't require
    bit for bit restarts.

--- src/clm-pflotran/pflotran_model.F90
+++ src/clm-pflotran/pflotran_model.F90
@@ -371,7 +371,7 @@ contains
   ! NOTE(bja, 2013-06-27) : the date stamp from clm is 32 characters
   !
   ! **************************************************************************
-  subroutine pflotranModelStepperCheckpoint(model, date_stamp)
+  subroutine pflotranModelStepperCheckpoint(model, id_stamp)
 
     use Option_module
     use Timestepper_module, only : StepperCheckpoint
@@ -379,14 +379,10 @@ contains
     implicit none
 
     type(pflotran_model_type), pointer :: model
-    character(len=MAXWORDLENGTH), intent(in) :: date_stamp
+    character(len=MAXWORDLENGTH), intent(in) :: id_stamp
+    PetscViewer :: viewer
 
-    model%option%io_buffer = 'checkpoint is not implemented for clm-pflotran.'
-    call printWrnMsg(model%option)
-!!$    call StepperCheckpoint(model%realization, &
-!!$         model%simulation%flow_stepper, &
-!!$         model%simulation%tran_stepper, &
-!!$         NEG_ONE_INTEGER, date_stamp)
+    call model%simulation%process_model_coupler_list%Checkpoint(viewer, -1, id_stamp)
 
   end subroutine pflotranModelStepperCheckpoint
 
@@ -1875,16 +1871,15 @@ end subroutine pflotranModelSetICs
     type(pflotran_model_type), pointer :: model
     character(len=MAXWORDLENGTH) :: restart_stamp
 
-    model%option%io_buffer = 'restart is not implemented in clm-pflotran.'
     call printWrnMsg(model%option)
 
-!!$    if (.not. StringNull(restart_stamp)) then
-!!$       model%option%restart_flag = PETSC_TRUE
-!!$       model%option%restart_filename = &
-!!$            trim(model%option%global_prefix) // &
-!!$            trim(model%option%group_prefix) // &
-!!$            '.' // trim(restart_stamp) // '.chk'
-!!$    end if
+    if (.not. StringNull(restart_stamp)) then
+       model%option%restart_flag = PETSC_TRUE
+       model%option%restart_filename = &
+            trim(model%option%global_prefix) // &
+            trim(model%option%group_prefix) // &
+            '-' // trim(restart_stamp) // '.chk'
+    end if
 
   end subroutine pflotranModelSetupRestart
 
