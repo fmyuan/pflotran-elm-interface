84cd1d5b387eb753b9e24ee99c2f5299657111d3
Guoping Tang <tanggp@yahoo.com>
2013-10-15 09:11:45 -0400

    Add check for psi < minpsi in reaction_sandbox_clm_cnp.F90 for moisture response function

--- src/pflotran/reaction_sandbox_clm_cnp.F90
+++ src/pflotran/reaction_sandbox_clm_cnp.F90
@@ -800,7 +800,15 @@ subroutine CLM_CNPReact(this,Residual,Jacobian,compute_derivative, &
 
   maxpsi = sucsat_pf_loc(local_id) * (-9.8d-6)
   psi = min(soilpsi_pf_loc(local_id), maxpsi)
-  F_theta = log(minpsi/psi)/log(minpsi/maxpsi)
+
+  if(psi > minpsi) then
+     F_theta = log(minpsi/psi)/log(minpsi/maxpsi)
+  else
+     F_theta = 0.0d0
+     call VecRestoreArrayReadF90(clm_pf_idata%sucsat_pf, sucsat_pf_loc, ierr)
+     call VecRestoreArrayReadF90(clm_pf_idata%soilpsi_pf, soilpsi_pf_loc, ierr)
+     return
+  endif
 
   call VecRestoreArrayReadF90(clm_pf_idata%sucsat_pf, sucsat_pf_loc, ierr)
   call VecRestoreArrayReadF90(clm_pf_idata%soilpsi_pf, soilpsi_pf_loc, ierr)
