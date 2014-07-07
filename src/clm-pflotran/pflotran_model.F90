14a76f908a4917530857b9696f1075e32702137c
Fengming Yuan <fmyuan@gmail.com>
2014-07-07 09:21:22 -0400

    write to 'fidout' file messages, CLM waypoint inserting and if CLM turns-off h5 output at input-deck defined intervals.

--- src/clm-pflotran/pflotran_model.F90
+++ src/clm-pflotran/pflotran_model.F90
@@ -1940,7 +1940,8 @@ end subroutine pflotranModelSetICs
     PetscReal :: pause_time1
 
     if (model%option%io_rank == model%option%myrank) then
-       write(model%option%fid_out, *) '>>>> Inserting waypoint at pause_time = ', pause_time
+       write(model%option%fid_out, *) '>>>> Inserting waypoint at pause_time (s) = ', pause_time
+       write(model%option%fid_out, *) '>>>> for CLM timestep: ', pause_time/dtime
     endif
 
     pause_time1 = pause_time + dtime!1800.0d0
@@ -2113,6 +2114,10 @@ end subroutine pflotranModelSetICs
 
     ! turn off the 'print out' if required from CLM
     if(.not.isprintout) then
+      if (model%option%io_rank == model%option%myrank) then
+        write(model%option%fid_out, *) 'NOTE: h5 output at input-defined interval from PFLOTRAN IS OFF! '
+      endif
+
        waypoint => realization%waypoints%first
        do
            if (.not.associated(waypoint)) exit
