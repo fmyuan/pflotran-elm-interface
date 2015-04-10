743ed087cae74fcfb9fba8a424d679f6b87fb571
Fengming Yuan <fmyuan@gmail.com>
2015-04-10 13:04:56 -0400

    testing the dtmin option in sandboxes
    --HG--
    branch : default-bgc

--- src/pflotran/reaction_sandbox_plantn.F90
+++ src/pflotran/reaction_sandbox_plantn.F90
@@ -443,8 +443,8 @@ subroutine PlantNReact(this,Residual,Jacobian,compute_derivative, &
     ! in the following, '2.1' multiplier is chosen because that is slightly larger(5% to avoid numerical issue) than '2.0',
     ! which should be the previous-timestep before reaching the 'option%dt_min'
     ! (the time-cut used in PF is like dt=0.5*dt, when cutting)
-    dtmin = 2.1d0*option%dt_min
-    !dtmin = max(option%tran_dt, 2.1d0*option%dt_min)   ! this 'dtmin' may be accelerating the timing, but may not be appropriate to mulitple consummers
+    !dtmin = 2.1d0*option%dt_min
+    dtmin = max(option%tran_dt, 2.1d0*option%dt_min)   ! this 'dtmin' may be accelerating the timing, but may not be appropriate to mulitple consummers
 
     if (this%ispec_nh4 > 0) then
        nratecap = this%rate_plantndemand * dtmin
