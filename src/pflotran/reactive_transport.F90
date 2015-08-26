7e035c3733dfcca7e5c8039afee37d1f2805c81b
Fengming Yuan <fmyuan@gmail.com>
2015-08-26 17:33:03 -0400

    remove some scaling-down checking message writing - it costs a lot of computing time for large domain.
    --HG--
    branch : default-bgc

--- src/pflotran/reactive_transport.F90
+++ src/pflotran/reactive_transport.F90
@@ -387,8 +387,7 @@ subroutine RTCheckUpdatePre(line_search,C,dC,changed,realization,ierr)
     if (min_ratio < 1.d0) then
 
 !#ifdef DEBUG
-      if(realization%option%tran_dt < 2.0d0*realization%option%dt_min .or. &
-         min_ratio <= min_allowable_scale ) then
+      if(min_ratio < min_allowable_scale ) then
         write(realization%option%fid_out, *) '-----checking scaling factor for RT ------'
         write(realization%option%fid_out, *) 'min. scaling factor = ', min_ratio
         write(realization%option%fid_out, *) 'elapsed time = ', realization%option%time
@@ -402,14 +401,10 @@ subroutine RTCheckUpdatePre(line_search,C,dC,changed,realization,ierr)
             'rt species no. =',i-floor((i-1.d0)/j)*j, '-------------->'
           endif
 
-          write(realization%option%fid_out, *) 'i=', i, &
-            'cell_no=',floor((i-1.0d0)/j), &
-            'rt_species_no.=',i-floor((i-1.0d0)/j)*j, &
-            'C_p/dC_p=', ratio, 'C_p=',C_p(i),'dC_p=',dC_p(i)
         enddo
         write(realization%option%fid_out, *) '-----DONE: checking scaling factor for RT ----'
 
-        if (min_ratio<=min_allowable_scale) then
+        if (min_ratio<min_allowable_scale) then
           write(realization%option%fid_out, *) ' min_ratio IS too small to make sense, '// &
             'which less than an allowable_scale value !'
           write(realization%option%fid_out, *) ' STOP executing ! '
