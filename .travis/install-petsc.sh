57fc0e5850b9f548bae53d4d704e94fb8765af8e
Glenn Hammond <gehammo@sandia.gov>
2017-05-01 10:04:59 -0700

    Fixed compiler names since mpich wrappers must be compiled.

--- .travis/install-petsc.sh
+++ .travis/install-petsc.sh
@@ -11,7 +11,7 @@ git checkout ${PETSC_GIT_HASH}
 export PETSC_DIR=$PWD
 export PETSC_ARCH=petsc-arch
 
-./configure PETSC_ARCH=petsc-arch --with-cc=mpicc --with-dxx=mpicxx --with-fc=mpif90 --CFLAGS='-g -O0' --CXXFLAGS='-g -O0' --FFLAGS='-g -O0 -Wno-unused-function' --with-clanguage=c --with-debug=1 --with-shared-libraries=0 --download-hdf5 --download-metis --download-parmetis --download-fblaslapack --download-mpich=http://www.mpich.org/static/downloads/3.2/mpich-3.2.tar.gz
+./configure PETSC_ARCH=petsc-arch --with-cc=gcc --with-cxx=g++ --with-fc=gfortran --CFLAGS='-g -O0' --CXXFLAGS='-g -O0' --FFLAGS='-g -O0 -Wno-unused-function' --with-clanguage=c --with-debug=1 --with-shared-libraries=0 --download-hdf5 --download-metis --download-parmetis --download-fblaslapack --download-mpich=http://www.mpich.org/static/downloads/3.2/mpich-3.2.tar.gz
 
 make
 
