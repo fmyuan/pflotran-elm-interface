e44b20fa4cb255b1741ab7097d33fedfc67e76c4
Jenn Frederick <jmfrede@sandia.gov>
2017-06-12 10:01:42 -0600

    KRP4 sat-pc fix: 1. Rearranged "if" logic to be the same ordering as the bragflo structure. 2. Removed if(S>=1) pc=0 statement.

--- src/pflotran/characteristic_curves.F90
+++ src/pflotran/characteristic_curves.F90
@@ -4778,15 +4778,13 @@ subroutine SF_KRP4_CapillaryPressure(this,liquid_saturation, &
   
   Se2 = (liquid_saturation-this%Sr)/(1.d0-this%Sr-this%Srg)
   
-  if (liquid_saturation <= this%Sr) then
+  if (((1.d0-liquid_saturation) <= this%Srg) .or. &
+      (liquid_saturation > this%Sr)) then
+    capillary_pressure = this%pct/(Se2**(1.d0/this%lambda))
+  else
     capillary_pressure = 0.d0
-    return
-  else if (liquid_saturation >= 1.d0) then
-    capillary_pressure = 0.d0
-    return
   endif
   
-  capillary_pressure = this%pct/(Se2**(1.d0/this%lambda))
   capillary_pressure = min(capillary_pressure,this%pcmax)
   
 end subroutine SF_KRP4_CapillaryPressure
