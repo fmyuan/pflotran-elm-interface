07b08c08db6349123dfb7d321353d84902d8699c
Fengming Yuan <fmyuan@gmail.com>
2017-09-28 16:53:01 -0400

    Allow TH mode to use Characteristic_Curves module (-DTH_CHARACTERISTIC_CURVES). (1) add CC dry-end smoothing (-DSMOOTHING2). (2) add ice pressure submodule in CC module. (3) in ice pressure submodule, add one more TH FREEZING option: PAINTER_KARRA_EXPLICIT_SMOOTH. (4) allow TH mode to read initial T and P data from hd5 file. (5) input error checking.

--- src/pflotran/patch.F90
+++ src/pflotran/patch.F90
@@ -2873,6 +2873,7 @@ subroutine PatchUpdateCouplerAuxVarsMPH(patch,coupler,option)
   if (associated(flow_condition%saturation)) then
     call SaturationUpdateCoupler(coupler,option,patch%grid, &
                                  patch%saturation_function_array, &
+                                 patch%characteristic_curves_array, &
                                  patch%sat_func_id)
   endif
 
@@ -2975,6 +2976,7 @@ subroutine PatchUpdateCouplerAuxVarsIMS(patch,coupler,option)
   if (associated(flow_condition%saturation)) then
     call SaturationUpdateCoupler(coupler,option,patch%grid, &
                                  patch%saturation_function_array, &
+                                 patch%characteristic_curves_array, &
                                  patch%sat_func_id)
   endif
 
@@ -3076,6 +3078,7 @@ subroutine PatchUpdateCouplerAuxVarsFLASH2(patch,coupler,option)
   if (associated(flow_condition%saturation)) then
     call SaturationUpdateCoupler(coupler,option,patch%grid, &
                                  patch%saturation_function_array, &
+                                 patch%characteristic_curves_array, &
                                  patch%sat_func_id)
   endif
 
@@ -3336,6 +3339,7 @@ subroutine PatchUpdateCouplerAuxVarsTH(patch,coupler,option)
   if (associated(flow_condition%saturation)) then
     call SaturationUpdateCoupler(coupler,option,patch%grid, &
                                  patch%saturation_function_array, &
+                                 patch%characteristic_curves_array, &
                                  patch%sat_func_id)
   endif
 
@@ -3495,6 +3499,7 @@ subroutine PatchUpdateCouplerAuxVarsRich(patch,coupler,option)
   if (associated(flow_condition%saturation)) then
     call SaturationUpdateCoupler(coupler,option,patch%grid, &
                                  patch%saturation_function_array, &
+                                 patch%characteristic_curves_array, &
                                  patch%sat_func_id)
   endif
   if (associated(flow_condition%rate)) then
