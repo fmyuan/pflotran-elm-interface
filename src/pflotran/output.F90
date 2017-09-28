d89de9c4e4fdaf2ca9135d306642815382d14247
Fengming Yuan <fmyuan@gmail.com>
2017-09-28 17:19:22 -0400

    Specific Modification for coupling PFLOTRAN and CLM (-DCLM_PFLOTRAN): (2) avoid large ascii log file or screen printing, when coupled code runs.

--- src/pflotran/output.F90
+++ src/pflotran/output.F90
@@ -1305,7 +1305,9 @@ subroutine Output(realization_base,snapshot_plot_flag,observation_plot_flag, &
       call PetscTime(tend,ierr);CHKERRQ(ierr)
       write(option%io_buffer,'(f10.2," Seconds to write HDF5 file.")') &
             tend-tstart
-      call PrintMsg(option)
+#ifndef CLM_PFLOTRAN
+      call printMsg(option)
+#endif
     endif
    
     if (realization_base%output_option%print_tecplot) then
@@ -2456,7 +2458,9 @@ subroutine OutputAvegVars(realization_base)
       call PetscLogEventEnd(logging%event_output_hdf5,ierr);CHKERRQ(ierr)
       call PetscTime(tend,ierr);CHKERRQ(ierr)
       write(option%io_buffer,'(f10.2," Seconds to write HDF5 file.")') tend-tstart
-      call PrintMsg(option)
+#ifndef CLM_PFLOTRAN
+      call printMsg(option)
+#endif
     endif
 
     ! Reset the vectors to zero
