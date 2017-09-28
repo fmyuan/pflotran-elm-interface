dc1172b69f01b91873828d04a74f2f45fe9d40b7
Fengming Yuan <fmyuan@gmail.com>
2017-09-28 17:23:28 -0400

    Specific Modification for coupling PFLOTRAN and CLM (-DCLM_PFLOTRAN): (4) Allow vertically only reactive transport.

--- src/pflotran/reactive_transport.F90
+++ src/pflotran/reactive_transport.F90
@@ -407,7 +407,6 @@ subroutine RTComputeMassBalance(realization,max_size,sum_mol)
   use Field_module
   use Grid_module
 
-
   type(realization_subsurface_type) :: realization
   PetscInt :: max_size
   PetscReal :: sum_mol(max_size,8)
@@ -2349,6 +2348,9 @@ subroutine RTResidualFlux(snes,xx,r,realization,ierr)
   PetscReal :: msrc(1:realization%option%nflowspec)
   PetscInt :: icomp, ieqgas
 
+  ! zero out xy-direction transport
+  PetscReal :: unitvec_xyz(3)
+
   option => realization%option
   field => realization%field
   patch => realization%patch
@@ -2382,6 +2384,9 @@ subroutine RTResidualFlux(snes,xx,r,realization,ierr)
   vol_frac_prim = 1.d0
 
   ! Interior Flux Terms -----------------------------------
+  unitvec_xyz(1:2) = 0.d0
+  unitvec_xyz(3)   = 1.d0
+
   connection_set_list => grid%internal_connection_set_list
   cur_connection_set => connection_set_list%first
   sum_connection = 0  
@@ -2398,6 +2403,12 @@ subroutine RTResidualFlux(snes,xx,r,realization,ierr)
 
       if (patch%imat(ghosted_id_up) <= 0 .or.  &
           patch%imat(ghosted_id_dn) <= 0) cycle
+
+      !----------------
+      if ( option%flow%only_vertical_flow .or. option%transport%only_vertical_tran ) then
+         if(abs(dot_product(cur_connection_set%dist(1:3,iconn),unitvec_xyz)) < 1.d-20) cycle
+      end if
+      !----------------
 
       ! TFluxCoef will eventually be moved to another routine where it should be
       ! called only once per flux interface at the beginning of a transport
@@ -2439,6 +2450,12 @@ subroutine RTResidualFlux(snes,xx,r,realization,ierr)
         istart = iend-reaction%ncomp+1
         r_p(istart:iend) = r_p(istart:iend) - Res(1:reaction%ncomp)
       endif
+
+      if (associated(patch%internal_tran_fluxes)) then
+        patch%internal_tran_fluxes(1:reaction%ncomp,iconn) = &
+            Res(1:reaction%ncomp)
+      endif
+
 #else
       call TFluxCoef_CD(option,cur_connection_set%area(iconn), &
                  patch%internal_velocities(:,sum_connection), &
@@ -2463,11 +2480,15 @@ subroutine RTResidualFlux(snes,xx,r,realization,ierr)
         istart = iend-reaction%ncomp+1
         r_p(istart:iend) = r_p(istart:iend) + Res_2(1:reaction%ncomp)
       endif
-#endif
+
       if (associated(patch%internal_tran_fluxes)) then
         patch%internal_tran_fluxes(1:reaction%ncomp,iconn) = &
-            Res(1:reaction%ncomp)
+            Res_1(1:reaction%ncomp) + Res_2(1:reaction%ncomp)
       endif
+
+#endif
+
+
     enddo
     cur_connection_set => cur_connection_set%next
   enddo
@@ -2519,6 +2540,10 @@ subroutine RTResidualFlux(snes,xx,r,realization,ierr)
 !          rt_auxvars(ghosted_id)%mass_balance_delta(:,iphase) + Res
         endif  
 
+      if (associated(patch%boundary_tran_fluxes)) then
+        patch%boundary_tran_fluxes(1:reaction%ncomp,sum_connection) = &
+            -Res(1:reaction%ncomp)
+
 #else
       call TFluxCoef_CD(option,cur_connection_set%area(iconn), &
                 patch%boundary_velocities(:,sum_connection), &
@@ -2545,10 +2570,12 @@ subroutine RTResidualFlux(snes,xx,r,realization,ierr)
 !          rt_auxvars(ghosted_id)%mass_balance_delta(:,iphase) + Res
         endif  
       
-#endif                   
       if (associated(patch%boundary_tran_fluxes)) then
         patch%boundary_tran_fluxes(1:reaction%ncomp,sum_connection) = &
-            Res(1:reaction%ncomp)
+            -Res_2(1:reaction%ncomp)
+
+#endif
+
       endif
     enddo
     boundary_condition => boundary_condition%next
@@ -3189,6 +3216,9 @@ subroutine RTJacobianFlux(snes,xx,A,B,realization,ierr)
   PetscReal :: Res(realization%reaction%ncomp)  
 #endif
 
+  ! zero out xy-direction transport
+  PetscReal :: unitvec_xyz(3)
+
   option => realization%option
   field => realization%field
   patch => realization%patch  
@@ -3207,6 +3237,9 @@ subroutine RTJacobianFlux(snes,xx,A,B,realization,ierr)
   vol_frac_prim = 1.d0
 
   ! Interior Flux Terms -----------------------------------
+  unitvec_xyz(1:2) = 0.d0
+  unitvec_xyz(3)   = 1.d0
+
   ! must zero out Jacobian blocks
 
   call PetscLogEventBegin(logging%event_rt_jacobian_flux,ierr);CHKERRQ(ierr)
@@ -3227,6 +3260,12 @@ subroutine RTJacobianFlux(snes,xx,A,B,realization,ierr)
 
       if (patch%imat(ghosted_id_up) <= 0 .or.  &
           patch%imat(ghosted_id_dn) <= 0) cycle
+
+      !----------------
+      if ( option%flow%only_vertical_flow .or. option%transport%only_vertical_tran ) then
+         if(abs(dot_product(cur_connection_set%dist(1:3,iconn),unitvec_xyz)) < 1.d-20) cycle
+      end if
+      !----------------
 
       if (option%use_mc) then
         vol_frac_prim = rt_sec_transport_vars(local_id_up)%epsilon
@@ -4696,6 +4735,9 @@ subroutine RTExplicitAdvection(realization)
   PetscErrorCode :: ierr
   PetscViewer :: viewer
 
+  ! zero out xy-direction transport
+  PetscReal :: unitvec_xyz(3)
+
   procedure (TFluxLimiterDummy), pointer :: TFluxLimitPtr
   
   select case(realization%option%transport%tvd_flux_limiter)
@@ -4813,6 +4855,9 @@ subroutine RTExplicitAdvection(realization)
   endif
   
 ! Interior Flux Terms -----------------------------------
+  unitvec_xyz(1:2) = 0.d0
+  unitvec_xyz(3)   = 1.d0
+
   call VecGetArrayF90(field%tvd_ghosts,tvd_ghosts_p,ierr);CHKERRQ(ierr)
   connection_set_list => grid%internal_connection_set_list
   cur_connection_set => connection_set_list%first
@@ -4830,6 +4875,12 @@ subroutine RTExplicitAdvection(realization)
 
       if (patch%imat(ghosted_id_up) <= 0 .or.  &
           patch%imat(ghosted_id_dn) <= 0) cycle
+
+      !----------------
+      if ( option%flow%only_vertical_flow .or. option%transport%only_vertical_tran ) then
+         if(abs(dot_product(cur_connection_set%dist(1:3,iconn),unitvec_xyz)) < 1.d-20) cycle
+      end if
+      !----------------
         
       if (associated(cur_connection_set%id_dn2)) then
         id_up2 = cur_connection_set%id_up2(iconn)
