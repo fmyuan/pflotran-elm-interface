51bcd29f6be68d8babb0fe6951fd02124f43effe
Fengming Yuan <fmyuan@gmail.com>
2017-09-28 17:21:57 -0400

    Specific Modification for coupling PFLOTRAN and CLM (-DCLM_PFLOTRAN): (3) remove the max. time step limit when coupling runs.

--- src/pflotran/timestepper_base.F90
+++ src/pflotran/timestepper_base.F90
@@ -657,11 +657,15 @@ subroutine TimestepperBaseSetTargetTime(this,sync_time,option,stop_flag, &
     this%num_contig_revert_due_to_sync = 0
   endif
 
-
+! if coupled with CLM, max_time_step IS unlimited
+! because 'waypoint' is controled by the interface
+! otherwise, PF will stop at some point but CLM not-yet done
+#ifndef CLM_PFLOTRAN
   if (cumulative_time_steps >= max_time_step-1) then
     nullify(cur_waypoint)
     stop_flag = TS_STOP_MAX_TIME_STEP
   endif
+#endif
 
   ! update maximum time step size to current waypoint value
   if (associated(cur_waypoint)) then
