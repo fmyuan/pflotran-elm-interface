62e201d37656556ac5fe625be30f3a1715bca89a
Fengming Yuan <fmyuan@gmail.com>
2017-09-28 17:00:38 -0400

    Specific Modification for coupling PFLOTRAN and CLM (-DCLM_PFLOTRAN): (1) domain mapping, either by a set of ascii files, or by directly overridden by CLM mesh. And adjust inter-cell distance by elevation if inputting from CLM.

--- src/pflotran/factory_subsurface.F90
+++ src/pflotran/factory_subsurface.F90
@@ -6,6 +6,11 @@ module Factory_Subsurface_module
 
   use PFLOTRAN_Constants_module
   use Utility_module, only : Equal
+  
+
+#ifdef CLM_PFLOTRAN
+  use clmpf_interface_data, only : clm_pf_idata
+#endif
 
   implicit none
 
@@ -1943,6 +1948,12 @@ subroutine SubsurfaceReadRequiredCards(simulation,input)
   use NW_Transport_Aux_module
   use Init_Common_module
 
+
+#ifdef CLM_PFLOTRAN
+  use clmpf_interface_data
+#endif
+
+
   implicit none
 
   class(simulation_subsurface_type) :: simulation
@@ -1975,6 +1986,22 @@ subroutine SubsurfaceReadRequiredCards(simulation,input)
 
   call InputPushBlock(input,'SUBSURFACE',option)
 
+!.........................................................................
+!-------------------
+  ! when coupling with CLM, need to know if meshmaps are provided prior to 'GRID' reading
+  ! if not, CLM mesh will directly over-ride whatever in PF input card
+#ifdef CLM_PFLOTRAN
+  string = "MAPPING_FILES"
+  call InputFindStringInFile(input,option,string)
+  if (.not.InputError(input)) then
+    option%mapping_files = PETSC_TRUE
+    option%io_buffer = ' CLM-PF grid/mesh mapping files will be USED!'
+    call printMsg(option)
+  end if
+  rewind(input%fid)
+#endif
+!-------------------
+  
   ! GRID information - GRID is a required card for every simulation
   string = "GRID"
   call InputFindStringInFile(input,option,string)
@@ -2047,6 +2074,19 @@ subroutine SubsurfaceReadRequiredCards(simulation,input)
           call InputReadInt(input,option,grid%structured_grid%npz)
           call InputDefaultMsg(input,option,'npz')
 
+#ifdef CLM_PFLOTRAN
+          if (.not. option%mapping_files) then
+            ! note that, if coupled with CLM, CLM land domain configuration
+            ! will over-ride 'npx/npy/npz' read above
+            option%io_buffer = ' CLM land mpi configuration will over-ride PF'
+            call printMsg(option)
+
+            grid%structured_grid%npx = clm_pf_idata%npx
+            grid%structured_grid%npy = clm_pf_idata%npy
+            grid%structured_grid%npz = clm_pf_idata%npz
+          endif
+#endif
+ 
           if (option%myrank == option%io_rank .and. &
               option%print_to_screen) then
             option%io_buffer = ' Processor Decomposition:'
@@ -2319,6 +2359,14 @@ subroutine SubsurfaceReadInput(simulation,input)
     call PrintMsg(option)
 
     select case(trim(card))
+
+#ifdef CLM_PFLOTRAN
+!....................
+      case ('MAPPING_FILES')
+        call InputSkipToEND(input,option,'')
+        ! skip 'MAPPING_FILES ... END' block, which will read in pflotran_clm_setmapping.F90
+        ! needed, otherwise model crashes
+#endif
 
 !....................
       case ('GRID')
