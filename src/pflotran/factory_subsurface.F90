4d513dd6f09b28550be472a1fd318c5603890ea9
Fengming Yuan <fmyuan@gmail.com>
2017-10-04 15:24:54 -0400

    Specific Modification for coupling PFLOTRAN and CLM (-DCLM_PFLOTRAN): (7) add one more variables in 'option.F90'.

--- src/pflotran/factory_subsurface.F90
+++ src/pflotran/factory_subsurface.F90
@@ -3468,6 +3468,7 @@ subroutine SubsurfaceReadInput(simulation,input)
           endif
         endif
         if (Initialized(dt_min)) then
+          option%dt_min = dt_min
           if (associated(flow_timestepper)) then
             flow_timestepper%dt_min = dt_min
           endif
