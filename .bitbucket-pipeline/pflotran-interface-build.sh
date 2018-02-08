443a7f80fc8a3269bfa187b5b8c673ba32f63990
Gautam Bisht <gbisht@lbl.gov>
2018-02-08 09:45:43 -0800

    Minor changes to building pflotran_interface

--- .bitbucket-pipeline/pflotran-interface-build.sh
+++ .bitbucket-pipeline/pflotran-interface-build.sh
@@ -4,5 +4,6 @@ export PETSC_DIR=$PWD/petsc;
 export PETSC_ARCH=linux-gnu
 
 cd src/clm-pflotran;
+./link_files.sh;
 make pflotran_interface;
 
--- .travis/pflotran-interface-build.sh
+++ .travis/pflotran-interface-build.sh
@@ -6,5 +6,6 @@ export PETSC_ARCH=petsc-arch
 ls -l ${PETSC_DIR}/${PETSC_ARCH}/lib
 
 cd src/clm-pflotran;
+./link_files.sh;
 make pflotran_interface;
 
