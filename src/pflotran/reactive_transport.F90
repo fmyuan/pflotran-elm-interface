78fd3bfb52b385a9d08db3875a41c51d1580b72a
Yuan, Fengming <fmyuan@gmail.com>
2018-02-20 21:20:57 -0500

    Specific Modification for coupling PFLOTRAN and CLM Biogeochemistry: (5) bug fixes for reactive-transport when no flow mode defined.

--- src/pflotran/reactive_transport.F90
+++ src/pflotran/reactive_transport.F90
@@ -2560,6 +2560,12 @@ subroutine RTResidualFlux(snes,xx,r,realization,ierr)
         istart = iend-reaction%ncomp+1
         r_p(istart:iend) = r_p(istart:iend) - Res(1:reaction%ncomp)
       endif
+
+      if (associated(patch%internal_tran_fluxes)) then
+        patch%internal_tran_fluxes(1:reaction%ncomp,iconn) = &
+            Res(1:reaction%ncomp)
+      endif
+
 #else
       call TFluxCoef_CD(rt_parameter,option, &
                  global_auxvars(ghosted_id_up), &
@@ -2587,11 +2593,15 @@ subroutine RTResidualFlux(snes,xx,r,realization,ierr)
         istart = iend-reaction%ncomp+1
         r_p(istart:iend) = r_p(istart:iend) + Res_2(1:reaction%ncomp)
       endif
-#endif
+
       if (associated(patch%internal_tran_fluxes)) then
         patch%internal_tran_fluxes(1:reaction%ncomp,iconn) = &
-            Res(1:reaction%ncomp)
+            Res_1(1:reaction%ncomp) + Res_2(1:reaction%ncomp)
       endif
+
+#endif
+
+
     enddo
     cur_connection_set => cur_connection_set%next
   enddo
@@ -2676,11 +2686,13 @@ subroutine RTResidualFlux(snes,xx,r,realization,ierr)
 !          rt_auxvars(ghosted_id)%mass_balance_delta(:,iphase) + Res
         endif  
       
-#endif                   
       if (associated(patch%boundary_tran_fluxes)) then
         patch%boundary_tran_fluxes(1:reaction%ncomp,sum_connection) = &
-            Res(1:reaction%ncomp)
+            -Res_2(1:reaction%ncomp)
       endif
+
+#endif
+
     enddo
     boundary_condition => boundary_condition%next
   enddo
