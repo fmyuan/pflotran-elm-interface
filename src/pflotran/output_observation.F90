0d6f35fd572b357897d07a91b2fe3fc5ae2f0ec4
Yuan, Fengming <fmyuan@gmail.com>
2018-07-27 09:16:50 -0400

    Specific Modification for coupling PFLOTRAN and CLM Thermal-Hydrology (TH) MODE: (4) Add total water mass balance when mass balance print out option is on.

--- src/pflotran/output_observation.F90
+++ src/pflotran/output_observation.F90
@@ -2307,6 +2307,7 @@ subroutine OutputMassBalance(realization_base)
   PetscReal, allocatable :: sum_mol(:,:), sum_mol_global(:,:)
   
   PetscReal :: global_total_mass, global_water_mass
+  PetscReal :: sum_kg_water  ! sum of global water mass and fluxes
 
   PetscReal :: sum_trapped(realization_base%option%nphase)
   PetscReal :: sum_trapped_global(realization_base%option%nphase)
@@ -2369,11 +2370,13 @@ subroutine OutputMassBalance(realization_base)
         case(RICHARDS_MODE,RICHARDS_TS_MODE)
           call OutputWriteToHeader(fid,'Global Water Mass','kg','',icol)
           
-        case(TH_MODE,TH_TS_MODE)
+        case(TH_TS_MODE)
           call OutputWriteToHeader(fid,'Global Water Mass in Liquid Phase', &
                                     'kg','',icol)
-        case(G_MODE,H_MODE)
+        case(G_MODE,H_MODE, TH_MODE)
           call OutputWriteToHeader(fid,'Global Water Mass in Liquid Phase', &
+                                    'kg','',icol)
+          call OutputWriteToHeader(fid,'Global Water Mass in Solid Phase', &
                                     'kg','',icol)
           call OutputWriteToHeader(fid,'Global Air Mass in Liquid Phase', &
                                     'kg','',icol)
@@ -2504,6 +2507,10 @@ subroutine OutputMassBalance(realization_base)
             string = trim(coupler%name) // ' CO2 Mass'
             call OutputWriteToHeader(fid,string,units,'',icol)
         end select
+        if (option%nflowdof > 0) then
+          string = ' SUM of All Water Mass and Flux'
+          call OutputWriteToHeader(fid,string,'kg','',icol)
+        endif
         
         if (option%ntrandof > 0) then
           select case(option%itranmode)
@@ -2578,6 +2585,7 @@ subroutine OutputMassBalance(realization_base)
   if (option%nflowdof > 0) then
     sum_kg = 0.d0
     sum_trapped = 0.d0
+    sum_kg_water = 0.d0
     select type(realization_base)
       class is(realization_subsurface_type)
         select case(option%iflowmode)
@@ -2618,6 +2626,9 @@ subroutine OutputMassBalance(realization_base)
           do iphase = 1, option%nphase
             do ispec = 1, option%nflowspec
               write(fid,110,advance="no") sum_kg_global(ispec,iphase)
+
+              if(ispec==1) &
+              sum_kg_water = sum_kg_water + sum_kg_global(ispec,iphase)
             enddo
           enddo
         case(WF_MODE)
@@ -2797,6 +2808,11 @@ subroutine OutputMassBalance(realization_base)
           if (option%myrank == option%io_rank) then
             ! change sign for positive in / negative out
             write(fid,110,advance="no") -sum_kg_global
+
+            do iphase = 1, option%nphase
+              sum_kg_water = sum_kg_water+sum_kg_global(1, iphase)
+            enddo
+
           endif
 
           ! print out H2O flux
@@ -2833,6 +2849,10 @@ subroutine OutputMassBalance(realization_base)
           if (option%myrank == option%io_rank) then
             ! change sign for positive in / negative out
             write(fid,110,advance="no") -sum_kg_global
+
+            do iphase = 1, option%nphase
+              sum_kg_water = sum_kg_water+sum_kg_global(1, iphase)
+            enddo
           endif
 
           ! print out H2O flux
@@ -2911,6 +2931,11 @@ subroutine OutputMassBalance(realization_base)
           if (option%myrank == option%io_rank) then
             ! change sign for positive in / negative out
             write(fid,110,advance="no") -sum_kg_global(:,1)
+
+            do iphase = 1, option%nphase
+              sum_kg_water = sum_kg_water+sum_kg_global(1, iphase)
+            enddo
+
           endif
 
           ! print out H2O flux
@@ -2967,6 +2992,18 @@ subroutine OutputMassBalance(realization_base)
             write(fid,110,advance="no") -sum_kg_global(:,1)*output_option%tconv
           endif
       end select
+
+      ! sum of all mass-balance items
+      ! print out sum of global water mass and fluxes
+      ! (NOTE: THIS amount MUST BE constant, otherwise error in mass-conservation)
+      select case(option%iflowmode)
+        case(RICHARDS_MODE, G_MODE, TH_MODE)
+          if (option%myrank == option%io_rank) then
+            write(fid,110,advance="no") sum_kg_water
+          endif
+
+      end select
+
     endif
     
     if (option%ntrandof > 0) then
