e34657fdb9170ed1571810d2e183f46e399e1f40
David Ponting <davidkponting@gmail.com>
2019-01-21 14:14:53 +0000

    Add readers for the new ECLIPSE file switches and also 'WRITE_MASS_RATES'

--- src/pflotran/output.F90
+++ src/pflotran/output.F90
@@ -112,13 +112,13 @@ subroutine OutputFileRead(input,realization,output_option, &
   character(len=MAXWORDLENGTH) :: units, internal_units
   character(len=MAXSTRINGLENGTH) :: string
   PetscReal :: temp_real,temp_real2
-  PetscReal :: units_conversion
-  PetscInt :: k
+  PetscReal :: units_conversion,deltat
+  PetscInt :: k,deltas
   PetscBool :: added
   PetscBool :: vel_cent, vel_face
   PetscBool :: fluxes
   PetscBool :: mass_flowrate, energy_flowrate
-  PetscBool :: aveg_mass_flowrate, aveg_energy_flowrate
+  PetscBool :: aveg_mass_flowrate, aveg_energy_flowrate,is_sum,is_rst
 
   option => realization%option
   patch => realization%patch
@@ -176,7 +176,18 @@ subroutine OutputFileRead(input,realization,output_option, &
           case('MASS_BALANCE_FILE')
             output_option%print_initial_massbal = PETSC_FALSE
         end select
-        
+
+      case('WRITE_MASS_RATES')
+        select case(trim(block_name))
+          case('MASS_BALANCE_FILE')
+              option%write_masses = PETSC_TRUE
+        end select
+
+      case('FORMATTED')
+        select case(trim(block_name))
+          case('ECLIPSE_FILE')
+              option%write_ecl_form = PETSC_TRUE
+        end select
 !...............................
       case('TOTAL_MASS_REGIONS')
         select case(trim(block_name))
@@ -357,6 +368,46 @@ subroutine OutputFileRead(input,realization,output_option, &
             call InputKeywordUnrecognized(word,'OUTPUT,PERIODIC',option)
         end select
 
+      case('PERIOD_SUM','PERIOD_RST')
+        is_sum=StringCompareIgnoreCase(word,'PERIOD_SUM')
+        is_rst=StringCompareIgnoreCase(word,'PERIOD_RST')
+        string = 'OUTPUT,' // trim(block_name) // ',' //trim(word)
+        call InputReadWord(input,option,word,PETSC_TRUE)
+        call InputErrorMsg(input,option,'periodic time increment type',string)
+        call StringToUpper(word)
+        select case(trim(word))
+          case('TIME')
+            deltat=-1.0
+            string = 'OUTPUT,' // trim(block_name) // ',' //trim(word)// ',TIME'
+            call InputReadDouble(input,option,temp_real)
+            call InputErrorMsg(input,option,'time increment',string)
+            call InputReadWord(input,option,word,PETSC_TRUE)
+            call InputErrorMsg(input,option,'time increment units',string)
+            internal_units = 'sec'
+            units_conversion = UnitsConvertToInternal(word, &
+                 internal_units,option)
+            deltat = temp_real*units_conversion
+            if( is_sum ) then
+              option%write_ecl_sum_deltat=deltat
+              option%write_ecl_sum_deltas=-1
+            endif
+            if( is_rst ) then
+              option%write_ecl_rst_deltat=deltat
+              option%write_ecl_rst_deltas=-1
+            endif
+          case('TIMESTEP')
+            deltas=-1
+            string = 'OUTPUT,' // trim(block_name) // ',' //trim(word)// ',TIMESTEP'
+              call InputReadInt(input,option,deltas)
+            if( is_sum ) then
+              option%write_ecl_sum_deltas=deltas
+              option%write_ecl_sum_deltat=-1.0
+            endif
+            if( is_rst ) then
+              option%write_ecl_rst_deltas=deltas
+              option%write_ecl_rst_deltat=-1.0
+            endif
+        end select
 !...................
       case('SCREEN')
         string = 'OUTPUT,' // trim(block_name) // ',SCREEN'
