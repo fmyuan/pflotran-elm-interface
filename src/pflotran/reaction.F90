fdc98662a1803638d5ea4df9c7197754ac47d973
Yuan, Fengming <fmyuan@gmail.com>
2019-05-20 14:07:13 -0400

    NEW DEVELOPMENT: (8) SOMDEC sandbox modifications.  - allow explicitly specifying SOMDEC Oxidizer (O2) and CO2 species names for all or individual reaction.  - add MONOD and INHIBITION functions into SOMDEC sandboxes, either for immobilization or explicit microbial regulation in SOMDEC reactions.  - allow abiotic response function(s) assigned either for all or for individual reaction.

--- src/pflotran/reaction.F90
+++ src/pflotran/reaction.F90
@@ -3689,6 +3689,7 @@ subroutine RReactionDerivative(Res,Jac,rt_auxvar,global_auxvar, &
   PetscReal :: Jac_dummy(reaction%ncomp,reaction%ncomp)
   PetscReal :: pert
   PetscBool :: compute_derivative
+  PetscInt :: option_iflag
 
   if (global_auxvar%sat(LIQUID_PHASE) < rt_min_saturation) return
 
@@ -3707,8 +3708,10 @@ subroutine RReactionDerivative(Res,Jac,rt_auxvar,global_auxvar, &
   else ! numerical derivative
     compute_derivative = PETSC_FALSE
     Res_orig = 0.d0
+    option_iflag = option%iflag ! prior to hard reset below, save it for recovering later (F.-M. Yuan, 2019-06-11)
     option%iflag = 0 ! be sure not to allocate mass_balance array
     call RTAuxVarInit(rt_auxvar_pert,reaction,option)
+    option%iflag = option_iflag ! recover the original value (F.-M. Yuan, 2019-06-11)
     call RTAuxVarCopy(rt_auxvar_pert,rt_auxvar,option)
 
     call RReaction(Res_orig,Jac_dummy,compute_derivative,rt_auxvar, &
@@ -3747,15 +3750,17 @@ subroutine RReactionDerivative(Res,Jac,rt_auxvar,global_auxvar, &
                            (Res_pert(icomp)-Res_orig(icomp))/pert
       enddo
     enddo
-    
-    ! zero small derivatives
-    do icomp = 1, reaction%ncomp
-      do jcomp = 1, reaction%ncomp
-        if (dabs(Jac(icomp,jcomp)) < 1.d-40)  Jac(icomp,jcomp) = 0.d0
-      enddo
-    enddo
+
     call RTAuxVarStrip(rt_auxvar_pert)
-  endif
+
+  endif !
+    
+  ! zero small derivatives for both analytic and numerical jacobian
+  do icomp = 1, reaction%ncomp
+    do jcomp = 1, reaction%ncomp
+      if (dabs(Jac(icomp,jcomp)) < 1.d-40)  Jac(icomp,jcomp) = 0.d0
+    enddo
+  enddo
 
 end subroutine RReactionDerivative
 
