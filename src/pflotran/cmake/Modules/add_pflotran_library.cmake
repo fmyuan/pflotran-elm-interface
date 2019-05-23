d78719eac9c90fef48aa38e6a4c1cfcabce3643b
Yuan, Fengming <fmyuan@gmail.com>
2019-05-23 12:08:45 -0400

    Manually clean up codes of non-TH mode.

--- src/pflotran/cmake/Modules/add_pflotran_library.cmake
+++
@@ -1,7 +0,0 @@
-function(add_pflotran_library lib)
-  add_library(${lib} ${ARGN})
-  if (BUILD_SHARED_LIBS)
-    target_link_libraries(${lib} ${PFLOTRAN_LIBRARIES})
-  endif()
-endfunction(add_pflotran_library)
-
