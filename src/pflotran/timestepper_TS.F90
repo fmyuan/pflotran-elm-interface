0cfda5c9bb7862681fecae1062256f4fb260625f
Satish Karra <satkarra@lanl.gov>
2019-06-27 16:37:21 -0600

    Updated 543_flow_ts gold file since I had changed the 2nd derivative and IJacobian functions. Also, cleaned up timestepper_TS.F90

--- src/pflotran/timestepper_TS.F90
+++ src/pflotran/timestepper_TS.F90
@@ -212,8 +212,6 @@ subroutine TimestepperTSStepDT(this,process_model,stop_flag)
   Vec :: residual_vec
   PetscReal :: fnorm, inorm, scaled_fnorm
   PetscInt :: ts_reason
-  PetscInt :: num_newton_iter_before, num_newton_iter_after
-  PetscInt :: num_linear_iter_before, num_linear_iter_after
   PetscInt :: num_linear_iterations
   PetscInt :: num_newton_iterations
   PetscErrorCode :: ierr
@@ -239,23 +237,17 @@ subroutine TimestepperTSStepDT(this,process_model,stop_flag)
   call TSSetExactFinalTime(solver%ts,TS_EXACTFINALTIME_MATCHSTEP, &
                            ierr);CHKERRQ(ierr)
 
-  !call TSGetSNESIterations(solver%ts,num_newton_iter_before,ierr)
-  !call TSGetKSPIterations(solver%ts,num_linear_iter_before,ierr)
   call TSSolve(solver%ts,process_model%solution_vec,ierr);CHKERRQ(ierr)
 
   call TSGetConvergedReason(solver%ts,ts_reason,ierr);CHKERRQ(ierr)
   call TSGetTime(solver%ts,time,ierr);CHKERRQ(ierr)
   call TSGetTimeStep(solver%ts,dtime,ierr);CHKERRQ(ierr)
   call TSGetSNESIterations(solver%ts,num_newton_iterations,ierr)
-  !call TSGetSNESIterations(solver%ts,num_newton_iter_after,ierr)
   call TSGetKSPIterations(solver%ts,num_linear_iterations,ierr)
-  !call TSGetKSPIterations(solver%ts,num_linear_iter_after,ierr)
 
-  !this%num_newton_iterations = num_newton_iter_after - num_newton_iter_before
   this%num_newton_iterations = num_newton_iterations 
   this%cumulative_newton_iterations = this%cumulative_newton_iterations + this%num_newton_iterations
 
-  !this%num_linear_iterations = num_linear_iter_after - num_linear_iter_before
   this%num_linear_iterations = num_linear_iterations
   this%cumulative_linear_iterations = this%cumulative_linear_iterations + this%num_linear_iterations
 
