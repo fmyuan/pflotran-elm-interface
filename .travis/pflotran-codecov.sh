1711aaa8a931835ceb4a1220b166dc74c30dc0af
Glenn Hammond <gehammo@sandia.gov>
2019-09-09 14:34:18 -0700

    #2 attempt to fix codecov

--- .travis/pflotran-codecov.sh
+++ .travis/pflotran-codecov.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+MINIMAL_BUILD=0
 if [ $MINIMAL_BUILD -eq 0 ]; then
-  bash <(curl -s https://codecov.io/bash)
+  echo 'bash <(curl -s https://codecov.io/bash)'
+  bash -c "curl -s https://codecov.io/bash" > curl.stdout
+  cat curl.stdout
 fi
