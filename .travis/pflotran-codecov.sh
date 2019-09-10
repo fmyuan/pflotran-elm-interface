5739a961d6fa6a246990a1b8ce83fdbe1d0dcdb7
Glenn Hammond <gehammo@sandia.gov>
2019-09-09 20:48:10 -0700

    testing codecov

--- .travis/pflotran-codecov.sh
+++ .travis/pflotran-codecov.sh
@@ -2,7 +2,9 @@
 
 MINIMAL_BUILD=0
 if [ $MINIMAL_BUILD -eq 0 ]; then
-  echo 'bash <(curl -s https://codecov.io/bash)'
-  bash -c "curl -s https://codecov.io/bash" > curl.stdout
-  cat curl.stdout
+#  echo 'bash <(curl -s https://codecov.io/bash)'
+#  bash -c "curl -s https://codecov.io/bash" > curl.stdout
+#  cat curl.stdout
+  env > env.stdout
+  cat env.stdout
 fi
