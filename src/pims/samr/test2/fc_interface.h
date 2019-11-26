975b03e0f0fe691bcbb25e4152427ce34af80fbe
Yuan, Fengming <fmyuan@gmail.com>
2019-11-26 09:03:22 -0500

    remove non-Flow mode codes.

--- src/pims/samr/test2/fc_interface.h
+++
@@ -1,8 +0,0 @@
-#ifndef fc_interface_h
-#define fc_interface_h
-extern "C"{
-   void f_create_local_patch_data_(void **p_data);
-   void f_create_hierarchy_data_(void **p_data, void **p_hierarchy);
-}
-
-#endif
