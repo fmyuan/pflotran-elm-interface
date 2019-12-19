2a81433fcafb2661b553bbd779122973ff5fd300
Glenn Hammond <gehammo@sandia.gov>
2019-12-18 16:31:43 -0800

    updated osrt cfl limiting

--- src/pflotran/pm_osrt.F90
+++ src/pflotran/pm_osrt.F90
@@ -340,7 +340,7 @@ subroutine PMOSRTUpdateTimestep(this,dt,dt_min,dt_max,iacceleration, &
   dtt = max(dtt,dt_min)
   dt = dtt
   
-  call RealizationLimitDTByCFL(this%realization,this%cfl_governor,dt)
+  call RealizationLimitDTByCFL(this%realization,this%cfl_governor,dt,dt_max)
   
 end subroutine PMOSRTUpdateTimestep
 
