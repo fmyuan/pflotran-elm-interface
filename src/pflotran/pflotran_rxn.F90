144af6fbd5224cfd6e953b7b579944307358cf10
Glenn Hammond <gehammo@sandia.gov>
2020-05-20 12:52:52 -0700

    Removed copyright statement from pflotran_rxn.F90

--- src/pflotran/pflotran_rxn.F90
+++ src/pflotran/pflotran_rxn.F90
@@ -1,44 +1,6 @@
 !=======================================================================
-! PFLOTRAN v2.0 LA-CC-09-047
+! Please see LICENSE and COPYRIGHT files at top of repository.
 !=======================================================================
-
-!Copyright 2009. Los Alamos National Security, LLC. This material was produced under U.S. 
-!Government contract DE-AC52-06NA25396 for Los Alamos National Laboratory (LANL), which is operated 
-!by Los Alamos National Security, LLC for the U.S. Department of Energy. The U.S. Government has 
-!rights to use, reproduce, and distribute this software.  NEITHER THE GOVERNMENT NOR LOS ALAMOS 
-!NATIONAL SECURITY, LLC MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR ASSUMES ANY LIABILITY FOR THE 
-!USE OF THIS SOFTWARE.  If software is modified to produce derivative works, such modified software 
-!should be clearly marked, so as not to confuse it with the version available from LANL.
-!Additionally, this library is free software; you can redistribute it and/or modify it under the 
-!terms of the GNU Lesser General Public License as published by the Free Software Foundation; 
-!either version 2.1 of the License, or (at your option) any later version. Accordingly, this 
-!library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even 
-!the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser 
-!General Public License for more details.
-
-! Send all bug reports/questions/comments to:
-!
-! Peter C. Lichtner
-! Los Alamos National Laboratory
-! Earth and Environmental Sciences
-! EES-16, MS: D469
-! (505) 667-3420
-! lichtner@lanl.gov
-! Los Alamos, NM
-
-! or
-
-! Glenn E. Hammond
-! Sandia National Laboratories
-! Applied Systems Analysis & Research
-! 413 Cherry Blossom Lp
-! Richland, WA 99352
-! (505) 235-0665
-! gehammo@sandia.gov
-
-!=======================================================================
-
-
 module BatchChem
 
 #include "petsc/finclude/petscsys.h"
