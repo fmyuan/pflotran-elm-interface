38c37bb049a24ebf7f63afa8b6ed9eaa9f77f27d
Glenn Hammond <glenn.hammond@pnnl.gov>
2020-10-01 18:04:01 -0700

    Fixed progress bar for roundoff

--- src/python/common/progress_bar.py
+++ src/python/common/progress_bar.py
@@ -12,11 +12,17 @@ class ProgressBar():
         self.print_fraction = print_fraction
         self.print_increment = int(self.print_fraction*self.maximum_iteration)
         self.iteration = 0
-        # in the future, use print("",end='') avialable with python3
-        sys.stdout.write('Progress: 0%')
-        sys.stdout.flush()
+        self.print_flag = True
+        if self.maximum_iteration < 100:
+            self.print_flag = False
+        if self.print_flag:
+            # in the future, use print("",end='') avialable with python3
+            sys.stdout.write('Progress: 0%')
+            sys.stdout.flush()
         
     def increment(self):
+        if not self.print_flag:
+             return 
         self.iteration += 1
         if self.iteration == self.maximum_iteration or \
            self.iteration % self.print_increment == 0:
@@ -27,9 +33,13 @@ class ProgressBar():
         if self.iteration == self.maximum_iteration:
             sys.stdout.write('-100%\n')
         else:
-            percent = int(int(float(self.iteration) / self.maximum_iteration /
+            percent = int(int((1.e-4+
+                               float(self.iteration) / self.maximum_iteration) /
                               self.print_fraction)*self.print_fraction*100.)
             sys.stdout.write('-{}%'.format(percent))
+            if percent == 100:
+                self.print_flag = False
+                sys.stdout.write('\n')
         
         
         
