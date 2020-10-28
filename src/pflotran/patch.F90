0c5b6a9270dcbb12a7290268b1c243c77e5cf190
Heeho Park <heepark@sandia.gov>
2020-10-27 22:32:52 -0600

    SaturationUpdateCoupler moved to patch.F90 as PatchUpdateCouplerSaturation

--- src/pflotran/patch.F90
+++ src/pflotran/patch.F90
@@ -149,6 +149,7 @@ module Patch_module
             PatchGetCompMassInRegion, &
             PatchGetWaterMassInRegion, &
             PatchGetCompMassInRegionAssign, &
+            PatchUpdateCouplerSaturation, &
             PatchSetupUpwindDirection
 
 contains
@@ -3127,6 +3128,7 @@ subroutine PatchUpdateCouplerAuxVarsH(patch,coupler,option)
   endif
 
 end subroutine PatchUpdateCouplerAuxVarsH
+
 ! ************************************************************************** !
 
 subroutine PatchUpdateCouplerAuxVarsTOI(patch,coupler,option)
@@ -3841,7 +3843,7 @@ subroutine PatchUpdateCouplerAuxVarsMPH(patch,coupler,option)
     end select
   endif
   if (associated(flow_condition%saturation)) then
-    call SaturationUpdateCoupler(coupler,option,patch%grid, &
+    call PatchUpdateCouplerSaturation(coupler,option,patch%grid, &
                                  patch%characteristic_curves_array, &
                                  patch%cc_id)
   endif
@@ -3942,7 +3944,7 @@ subroutine PatchUpdateCouplerAuxVarsIMS(patch,coupler,option)
     end select
   endif
   if (associated(flow_condition%saturation)) then
-    call SaturationUpdateCoupler(coupler,option,patch%grid, &
+    call PatchUpdateCouplerSaturation(coupler,option,patch%grid, &
                                  patch%characteristic_curves_array, &
                                  patch%cc_id)
   endif
@@ -4042,7 +4044,7 @@ subroutine PatchUpdateCouplerAuxVarsFLASH2(patch,coupler,option)
     end select
   endif
   if (associated(flow_condition%saturation)) then
-    call SaturationUpdateCoupler(coupler,option,patch%grid, &
+    call PatchUpdateCouplerSaturation(coupler,option,patch%grid, &
                                  patch%characteristic_curves_array, &
                                  patch%cc_id)
   endif
@@ -4317,7 +4319,7 @@ subroutine PatchUpdateCouplerAuxVarsTH(patch,coupler,option)
     end select
   endif
   if (associated(flow_condition%saturation)) then
-    call SaturationUpdateCoupler(coupler,option,patch%grid, &
+    call PatchUpdateCouplerSaturation(coupler,option,patch%grid, &
                                  patch%characteristic_curves_array, &
                                  patch%cc_id)
   endif
@@ -4489,7 +4491,7 @@ subroutine PatchUpdateCouplerAuxVarsRich(patch,coupler,option)
     end select
   endif
   if (associated(flow_condition%saturation)) then
-    call SaturationUpdateCoupler(coupler,option,patch%grid, &
+    call PatchUpdateCouplerSaturation(coupler,option,patch%grid, &
                                  patch%characteristic_curves_array, &
                                  patch%cc_id)
   endif
@@ -4614,6 +4616,66 @@ subroutine PatchUpdateCouplerGridDataset(coupler,option,grid,dataset,dof)
   enddo
 
 end subroutine PatchUpdateCouplerGridDataset
+
+! ************************************************************************** !
+
+subroutine PatchUpdateCouplerSaturation(coupler,option,grid,cc_array,cc_id)
+  ! 
+  ! Computes the pressures for a saturation
+  ! initial/boundary condition
+  ! 
+  ! Author: Glenn Hammond
+  ! Date: 06/07/11
+  !
+  ! Author: Heeho Park 
+  ! Modified Date: 10/05/20
+  ! 
+
+  use Option_module
+  use Condition_module
+  use Connection_module
+  
+  implicit none
+
+  type(coupler_type) :: coupler
+  type(option_type) :: option
+  type(grid_type) :: grid
+  type(characteristic_curves_ptr_type), intent(in) :: cc_array(:)
+  class(characteristic_curves_type), pointer :: cc_ptr
+
+  PetscInt :: cc_id(:)
+  PetscInt :: local_id, ghosted_id, iconn
+  PetscReal :: saturation
+  PetscReal :: capillary_pressure
+  PetscReal :: dpc_dsat
+  PetscReal :: liquid_pressure
+  
+  type(flow_condition_type), pointer :: condition
+  
+  type(connection_set_type), pointer :: cur_connection_set
+  
+  condition => coupler%flow_condition
+
+  if (option%iflowmode /= RICHARDS_MODE) then
+    option%io_buffer = 'PatchUpdateCouplerSaturation is not set up for this flow mode.'
+    call PrintErrMsg(option)
+  endif
+  
+  ! in this case, the saturation is stored within concentration dataset
+  saturation = condition%saturation%dataset%rarray(1)
+
+  do iconn = 1, coupler%connection_set%num_connections
+    local_id = coupler%connection_set%id_dn(iconn)
+    ghosted_id = grid%nL2G(local_id)
+    cc_ptr => cc_array(cc_id(ghosted_id))%ptr
+
+    call cc_ptr%saturation_function%CapillaryPressure(saturation,&
+                                           capillary_pressure,dpc_dsat,option)
+    liquid_pressure = option%reference_pressure - capillary_pressure
+    coupler%flow_aux_real_var(1,iconn) = liquid_pressure
+  enddo
+
+end subroutine PatchUpdateCouplerSaturation
 
 ! ************************************************************************** !
 
