61fc31f726e865528942ccee91b9de8af0083518
Glenn Hammond <glenn.hammond@pnnl.gov>
2021-02-15 10:22:49 -0800

    first pass at adding coverage

--- .gitlab/test-pflotran.sh
+++ .gitlab/test-pflotran.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-cd src/pflotran
+export PFLOTRAN_DIR=$PWD
+export ARTIFACT_DIR=/tmp/test-pflotran
+
+cd $PFLOTRAN_DIR/src/pflotran
 
 # Run unit tests
 make codecov=1 utest
@@ -11,13 +14,22 @@ if [ $UNIT_EXIT_CODE -ne 0 ]; then
 fi
 
 # Run regression tests
-cd ../../regression_tests
+cd $PFLOTRAN_DIR/regression_tests
 
 make test
 REGRESSION_EXIT_CODE=$?
 if [ $REGRESSION_EXIT_CODE -ne 0 ]; then
   echo "Regression tests failed" >&2
 fi
+
+cd $PFLOTRAN_DIR/src/pflotran
+lcov --capture --directory . --output-file pflotran_coverage.info
+genhtml pflotran_coverage.info --output-directory $PFLOTRAN_DIR/coverage
+
+rm -Rf $ARTIFACT_DIR
+mkdir -p $ARTIFACT_DIR
+cp -R $PFLOTRAN_DIR/regression_tests $ARTIFACT_DIR
+cp -R $PFLOTRAN_DIR/coverage $ARTIFACT_DIR
 
 if [ $UNIT_EXIT_CODE -eq 0 ] && [ $REGRESSION_EXIT_CODE -eq 0 ]; then
   exit 0
