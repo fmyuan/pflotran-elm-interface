bf2dcfd1a386d7ed380cc33e4284ecdc8512fec3
Glenn Hammond <glenn.hammond@pnnl.gov>
2021-09-10 09:48:29 -0700

    fixed KSPSetup error where operators were not set

--- src/pflotran/pmc_linear.F90
+++ src/pflotran/pmc_linear.F90
@@ -155,6 +155,9 @@ subroutine PMCLinearSetupSolvers(this)
       option%io_buffer = 'Process model not supported by PMCLinearSetupSolvers'
       call PrintErrMsg(option)
   end select
+  ! have to set operators so that any KSPSetup() calls within 
+  ! SolverSetKSPOptions have access to Mats and Vecs
+  call KSPSetOperators(solver%ksp,solver%M,solver%Mpre,ierr);CHKERRQ(ierr)
   call SolverSetKSPOptions(solver,option)
 
 end subroutine PMCLinearSetupSolvers
