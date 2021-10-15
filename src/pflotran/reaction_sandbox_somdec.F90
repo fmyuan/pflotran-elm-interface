e66c266b98d39b23451755c6596d3203856e7f9e
Benjamin Sulman <sulmanbn@ornl.gov>
2021-10-15 16:26:05 -0400

    Reduce how much of available N can be immobilized in a single time step by each reaction

--- src/pflotran/reaction_sandbox_somdec.F90
+++ src/pflotran/reaction_sandbox_somdec.F90
@@ -1646,7 +1646,7 @@ subroutine SomDecReact(this,Residual,Jacobian,compute_derivative,rt_auxvar, &
     elseif (cur_abioticfactors%temperature_response_function == &
         TEMPERATURE_RESPONSE_FUNCTION_CLMCN) then
       f_t = GetTemperatureResponse(tc, &
-                                 cur_abioticfactors%temperature_response_function)
+                                 cur_abioticfactors%temperature_response_function,0.0d0)
     elseif (cur_abioticfactors%temperature_response_function == &
         TEMPERATURE_RESPONSE_FUNCTION_Q10 .or. &
             cur_abioticfactors%temperature_response_function == &
@@ -2760,7 +2760,8 @@ subroutine SomDecReact2(this,Residual,Jacobian,compute_derivative, reaction, &
   ! - implying tiny timestep in model, which potentially crashes model
 
   dtmin = option%tran_dt
-  nratecap = -crate_uc*this%mineral_n_stoich(irxn)*dtmin   ! positive with unit: moles
+  nratecap = -crate_uc*this%mineral_n_stoich(irxn)*dtmin/0.45d0   ! positive with unit: moles
+  ! BSulman: Adjusting nratecap to prevent N from being (almost) totally depleted which causes problems
 
   if(this%species_id_nh4 > 0) then
     if (nratecap*fnh4_inhibit_no3 > c_nh4*volume) then       ! c_nh4 unit: moles/m3 bulk soil
