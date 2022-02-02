e91148b5f25e6248e60de5e2245b53e22883e206
Glenn Hammond <glenn.hammond@pnnl.gov>
2022-02-02 11:44:05 -0800

    Refactored gitlab pipeline script to properly handle failures

--- .gitlab/test-pflotran-interface.sh
+++ .gitlab/test-pflotran-interface.sh
@@ -6,9 +6,20 @@ export ARTIFACT_DIR=/tmp/test-pflotran-interface
 
 cd $SRC_DIR
 
+REGRESSION_EXIT_CODE=-999
+
 # Run regression tests
-make test
-REGRESSION_EXIT_CODE=$?
+TEST_LOG='test.log'
+make test 2>&1 | tee $TEST_LOG
+if [ $(grep -c "Failed : \|Errors : " "$TEST_LOG") -ne 0 ]; then
+  echo "\n----- Regression tests failed -----\n" >&2
+  REGRESSION_EXIT_CODE=1
+elif [ $(grep -c " All tests passed." "$TEST_LOG") -ne 0 ]; then
+  echo "\n----- Regression tests succeeded -----\n" >&2
+  REGRESSION_EXIT_CODE=0
+else
+  echo "\n----- Regression tests produced unrecognized result -----\n" >&2
+fi
 
 lcov --capture --directory . --output-file pflotran_coverage.info
 genhtml pflotran_coverage.info --output-directory coverage
@@ -20,7 +31,6 @@ cp pflotran-tests-* $ARTIFACT_DIR/regression_tests/.
 cp -R ./coverage $ARTIFACT_DIR
 
 if [ $REGRESSION_EXIT_CODE -ne 0 ]; then
-  echo "Regression tests failed" >&2
   echo 'failed' > $ARTIFACT_DIR/status
 else
   echo 'success' > $ARTIFACT_DIR/status
