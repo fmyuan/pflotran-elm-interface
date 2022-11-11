fb260d5e56880c6218591115c77469bc3ed78f27
Heeho Park <heepark@sandia.gov>
2022-11-11 15:59:14 -0800

    modified spline.F90 to improve readability and avoid duplicate codes from the reference

--- src/pflotran/spline.F90
+++ src/pflotran/spline.F90
@@ -1,125 +1,141 @@
-  module spline_module
+module Spline_module
 
 #include "petsc/finclude/petscsys.h"
 
   public
 
-  contains
+contains
 
 ! ************************************************************************** !
 
-subroutine spline(x,y,n,y2)
+subroutine SplineSecondDeriv(t,s,n,s_dp)
 
-
-!----------------------description-------------------------------------!
-!
-!     cubic spline second derivative.
-!
-!     press, w.h., b.p. flannery, s.a. teukolsky, and w.t. vetterling.
-!     1986.  numerical recipes, the art of scientific computing,
-!     cambridge university press, cambridge.  pp. 86-89.
+  !
+  !     s_dp(1:n), s double prime, is the second derivative of the
+  !     interpolating function at the tabluated point t(n),
+  !     where s(1:n) = function(t(1:n))
+  !
+  !     Reference: press, w.h., b.p. flannery, s.a. teukolsky, and
+  !     w.t. vetterling.
+  !     1986.  numerical recipes, the art of scientific computing,
+  !     cambridge university press, cambridge.  pp. 86-89.
+  !
+  !     Modified by Heeho Park
+  !     Date: 11/10/2023
 
   use PFLOTRAN_Constants_module
 
-      implicit none
+  implicit none
 
-      PetscInt :: i,n,k
-      PetscReal :: x(n),y(n),y2(n),u(n)
-      PetscReal :: sig,p,qn,un
+  PetscInt :: i,n,k
+  PetscReal :: t(n),s(n),s_dp(n),temp_array(n)
+  PetscReal :: alpha,temp,bound1,bound2
 
-      y2(1) = 0.d0
-      u(1) = 0.d0
-      do i = 2,n-1
-        sig = (x(i)-x(i-1))/(x(i+1)-x(i-1))
-        p = sig*y2(i-1)+2.d0
-        y2(i) = (sig-1.d0)/p
-        u(i) = (6.d0*((y(i+1)-y(i))/(x(i+1)-x(i)) - &
-        (y(i)-y(i-1))/(x(i)-x(i-1)))/ &
-        (x(i+1)-x(i-1)) - sig*u(i-1))/p
-      enddo
-      qn = 0.d0
-      un = 0.d0
-      y2(n) = (un-qn*u(n-1))/(qn*y2(n-1)+1.d0)
-      do k = n-1,1,-1
-        y2(k) = y2(k)*y2(k+1)+u(k)
-      enddo
+  s_dp(1) = 0.d0
+  temp_array(1) = 0.d0
+  
+  do i = 2,n-1
+    alpha = (t(i)-t(i-1))/(t(i+1)-t(i-1))
+    temp = alpha*s_dp(i-1)+2.d0
+    s_dp(i) = (alpha-1.d0)/temp
+    temp_array(i) = (6.d0*((s(i+1)-s(i))/(t(i+1)-t(i)) - &
+                    (s(i)-s(i-1))/(t(i)-t(i-1)))/ &
+                    (t(i+1)-t(i-1)) - alpha*temp_array(i-1))/temp
+  enddo
+  
+  bound1 = 0.d0
+  bound2 = 0.d0
+  s_dp(n) = (bound2-bound1*temp_array(n-1))/(bound1*s_dp(n-1)+1.d0)
 
-      return
-      end subroutine spline
+  do k = n-1,1,-1
+    s_dp(k) = s_dp(k)*s_dp(k+1)+temp_array(k)
+  enddo
 
-! ************************************************************************** !
-
-subroutine splint(xa,ya,y2a,n,x,y)
-
-!     cubic spline interpolation.
-
-!     press, w.h., b.p. flannery, s.a. teukolsky, and w.t. vetterling.
-!     1986.  numerical recipes, the art of scientific computing,
-!     cambridge university press, cambridge.  pp. 86-89.
-
-
-      implicit none
-      PetscInt :: n,k,klo,khi
-      PetscReal :: xa(n),ya(n),y2a(n)
-      PetscReal :: h,a,b,x,y
-
-      klo = 1
-      khi = n
-   10 continue
-      if (khi-klo.gt.1) then
-        k = (khi+klo)/2
-        if (xa(k).gt.x) then
-          khi = k
-        else
-          klo = k
-        endif
-        goto 10
-      endif
-      h = xa(khi)-xa(klo)
-      a = (xa(khi)-x)/h
-      b = (x-xa(klo))/h
-      y = a*ya(klo)+b*ya(khi)+ &
-          ((a**3-a)*y2a(klo)+(b**3-b)*y2a(khi))*(h**2)/6.d0
-!     y1a = (ya(khi)-ya(klo))/h - &
-!           ((3.d+0*(a**2)-1.d+0)/6.d+0)*h*y2a(klo) + &
-!           ((3.d+0*(b**2)-1.d+0)/6.d+0)*h*y2a(khi)
-
-      return
-      end subroutine splint
+  return
+  
+end subroutine SplineSecondDeriv
 
 ! ************************************************************************** !
 
-subroutine locate(xx,n,x,j)
+subroutine SplineInterp(arr1,arr2,s_dp,n,t,s_int)
 
-!     given an array xx of length n, and given a value x, returns a
-!     value j such that x is between xx(j) and xx(j+1).  xx must be
-!     monotonic, either increasing or decreasing.  j=0 or j=n is
-!     returned to indicate that x is out of range.
-!
-!     press, w.h., b.p. flannery, s.a. teukolsky, and w.t. vetterling.
-!     1986.  numerical recipes, the art of scientific computing.
-!     cambridge university press, cambridge.
+  !     two arrays arr1(1:n) and arr2(1:n) of length n and the second
+  !     derivative from SplineSecondDeriv s_dp(1:n), give a value of t,
+  !     this returns cubic-spline interpolated value of s_int. 
+  !
+  !     Reference: press, w.h., b.p. flannery, s.a. teukolsky, and
+  !     w.t. vetterling.
+  !     1986.  numerical recipes, the art of scientific computing,
+  !     cambridge university press, cambridge.  pp. 86-89.
+  !
+  !     Modified by Heeho Park
+  !     Date: 11/10/2023
 
-      implicit none
-      PetscInt :: jl,ju,jm,j,n
-      PetscReal :: xx(n)
-      PetscReal :: x
+  implicit none
 
-      jl = 0
-      ju = n+1
-   10 continue
-      if (ju-jl.gt.1) then
-        jm = (ju+jl)/2
-        if ((xx(n).gt.xx(1)).eqv.(x.gt.xx(jm))) then
-          jl = jm
-        else
-          ju = jm
-        endif
-        goto 10
-      endif
-      j = jl
+  PetscInt :: n,k,lower,upper
+  PetscReal :: arr1(n),arr2(n),s_dp(n)
+  PetscReal :: denom,c1,c2,t,s_int
 
-      return
-      end subroutine locate
+  lower = 1
+  upper = n
+  
+  do while (upper-lower.gt.1)
+    k = (upper+lower)/2
+    if (arr1(k).gt.t) then
+      upper = k
+    else
+      lower = k
+    endif
+  end do
+  
+  denom = arr1(upper)-arr1(lower)
+  c1 = (arr1(upper)-t)/denom
+  c2 = (t-arr1(lower))/denom
+  s_int = c1*arr2(lower)+c2*arr2(upper)+ &
+          ((c1**3-c1)*s_dp(lower)+(c2**3-c2)*s_dp(upper))*(denom**2)/6.d0
 
-  end module spline_module
+  return
+
+end subroutine SplineInterp
+
+! ************************************************************************** !
+
+subroutine BisectionSearch(arr,n,val,ind)
+
+  !     given an array arr of length n, and given a value val, returns a
+  !     value ind such that val is between arr(ind) and arr(ind+1).
+  !     arr must be monotonic, either increasing or decreasing.
+  !     ind=0 or ind=n is returned to indicate that val is out of range.
+  !
+  !     Reference press, w.h., b.p. flannery, s.a. teukolsky, and
+  !     w.t. vetterling.
+  !     1986.  numerical recipes, the art of scientific computing.
+  !     cambridge university press, cambridge.
+  !
+  !     Modified by Heeho Park
+  !     Date: 11/10/2023
+
+  implicit none
+
+  PetscInt :: lower,upper,mid,ind,n
+  PetscReal :: arr(n)
+  PetscReal :: val
+
+  lower = 0
+  upper = n+1
+  do while (upper-lower.gt.1)
+    mid = (upper+lower)/2
+    if ((arr(n).gt.arr(1)).eqv.(val.gt.arr(mid))) then
+      lower = mid
+    else
+      upper = mid
+    endif
+  end do
+  ind = lower
+
+  return
+  
+end subroutine BisectionSearch
+
+end module Spline_module
