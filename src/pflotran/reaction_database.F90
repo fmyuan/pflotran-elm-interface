b65b4a6dd805b7a703058590f377fa5f5a66909f
Glenn Hammond <glenn.hammond@pnnl.gov>
2023-02-08 10:09:10 -0800

    updated the error message

--- src/pflotran/reaction_database.F90
+++ src/pflotran/reaction_database.F90
@@ -1118,8 +1118,9 @@ subroutine BasisInit(reaction,option)
   enddo
 
   if (icount /= ncomp_secondary) then
-    call PrintMsgNoAdvance(option,new_line('a') // &
-                 'Species with reactions associated with them in database:')
+    call PrintMsgNoAdvance(option,new_line('a') // 'Species read from the &
+      &reaction database with reactions associated with them in the &
+      &database:')
     cur_pri_aq_spec => reaction%primary_species_list
     do
       if (.not.associated(cur_pri_aq_spec)) exit
@@ -1145,16 +1146,24 @@ subroutine BasisInit(reaction,option)
       cur_gas_spec => cur_gas_spec%next
     enddo
     call PrintMsg(option,'')
+    option%io_buffer = 'The number of species read from the reaction &
+      &database with associated reactions in the database (' // &
+      StringWrite(icount) // ') does not match the number of secondary &
+      &aqueous species and gases in the problem (' // &
+      StringWrite(ncomp_secondary) // ').'
     if (icount < ncomp_secondary) then
-      option%io_buffer = 'Too few reactions read from database for &
-        &number of secondary species defined.'
+      option%io_buffer = trim(option%io_buffer) // ' Since the number of &
+        &species with associated reactions is lower, it is likely that &
+        &there is a missing secondary aqueous species or gas.'
     else
-      option%io_buffer = 'Too many reactions read from database for &
-        &number of secondary species defined.  Perhaps &
-        &DECOUPLED_EQUILIBRIUM_REACTIONS need to be defined?'
+      option%io_buffer = trim(option%io_buffer) // ' Since the number of &
+        &species with associated reactions is larger, it is likely that &
+        &there is a species from the database with a reaction associated &
+        &and no corresponding secondary aqueous or gas species. In that &
+        &case, a DECOUPLED_EQUILIBRIUM_REACTIONS block is likely needed.'
     endif
     option%io_buffer = trim(option%io_buffer) // &
-      ' One or more of the species above is the problem species.'
+      ' One or more of the species above is the problem.'
     call PrintErrMsg(option)
   endif
 
