f444e92975cb70cb666bd61c4cd0fa6eb503b6b8
Jennifer M. Frederick <jmfrede@sandia.gov>
2023-04-13 16:00:44 -0700

    Putting back the ZERO_INTEGER value for k in PMWellUpdateRates. However, doing this doesn't work in parallel because there are values in the well data structures for reservoir properties that haven't been shared with all well processes since passing in ZERO_INTEGER causes the MPI calls to be skipped. This still needs to be fixed.

--- src/pflotran/wipp_flow.F90
+++ src/pflotran/wipp_flow.F90
@@ -1291,8 +1291,7 @@ subroutine WIPPFloResidual(snes,xx,r,realization,pmwss_ptr,pmwell_ptr,ierr)
   if (wippflo_well_quasi_imp_coupled) then
     if (associated(pmwell_ptr)) then
       if (any(pmwell_ptr%well_grid%h_rank_id == option%myrank)) then
-        !call PMWellUpdateRates(pmwell_ptr,ZERO_INTEGER,ierr)
-        call PMWellUpdateRates(pmwell_ptr,-1,ierr)
+        call PMWellUpdateRates(pmwell_ptr,ZERO_INTEGER,ierr)
         if (pmwell_ptr%well_force_ts_cut == ZERO_INTEGER) then
           call PMWellCalcResidualValues(pmwell_ptr,r_p,ss_flow_vol_flux)
         endif
