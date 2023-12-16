9bc679db4120678dc947a17b0cc9752fec87e091
Glenn Hammond <glenn.hammond@pnnl.gov>
2023-12-15 16:04:04 -0800

    refactored pressure src/sink sandbox to have max rate and smoothstep transition

--- src/pflotran/srcsink_sandbox_pressure.F90
+++ src/pflotran/srcsink_sandbox_pressure.F90
@@ -17,8 +17,10 @@ module SrcSink_Sandbox_Pressure_class
 
   type, public, &
     extends(srcsink_sandbox_base_type) :: srcsink_sandbox_pressure_type
-    PetscReal :: rate_constant
-    PetscReal :: pressure(2)
+    PetscInt :: iphase
+    PetscReal :: pressure
+    PetscReal :: max_mass_rate
+    PetscReal :: pressure_span
   contains
     procedure, public :: ReadInput => PressureRead
     procedure, public :: Setup => PressureSetup
@@ -45,8 +47,10 @@ function PressureCreate()
 
   allocate(PressureCreate)
   call SSSandboxBaseInit(PressureCreate)
-  PressureCreate%rate_constant = 0.d0
+  PressureCreate%iphase = UNINITIALIZED_INTEGER
+  PressureCreate%max_mass_rate = UNINITIALIZED_DOUBLE
   PressureCreate%pressure = UNINITIALIZED_DOUBLE
+  PressureCreate%pressure_span = 1.d4
 
 end function PressureCreate
 
@@ -69,8 +73,7 @@ subroutine PressureRead(this,input,option)
   type(input_type), pointer :: input
   type(option_type) :: option
 
-  PetscInt :: i
-  character(len=MAXWORDLENGTH) :: word, internal_units
+  character(len=MAXWORDLENGTH) :: word, internal_units, word2
   PetscBool :: found
 
   call InputPushBlock(input,option)
@@ -89,37 +92,62 @@ subroutine PressureRead(this,input,option)
 
     select case(trim(word))
 
-      case('RATE_CONSTANT')
-        call InputReadDouble(input,option,this%rate_constant)
+      case('PRESSURE_SPAN')
+        call InputReadDouble(input,option,this%pressure_span)
         call InputErrorMsg(input,option,word, &
                            'SOURCE_SINK_SANDBOX,PRESSURE,'//trim(word))
+      case('PHASE')
+        call InputReadWord(input,option,word2,PETSC_TRUE)
+        call InputErrorMsg(input,option,word2, &
+                           'SOURCE_SINK_SANDBOX,PRESSURE,'//trim(word))
+        call StringToUpper(word2)
+        select case(word2)
+          case('LIQUID')
+            this%iphase = option%liquid_phase
+          case('GAS')
+            this%iphase = option%gas_phase
+          case default
+            call InputKeywordUnrecognized(input,word2, &
+                                'SRCSINK_SANDBOX,PRESSURE,PHASE',option)
+        end select
+      case('MAXIMUM_MASS_RATE')
+        call InputReadDouble(input,option,this%max_mass_rate)
+        call InputErrorMsg(input,option,word, &
+                           'SOURCE_SINK_SANDBOX,PRESSURE,'//trim(word))
+        internal_units = 'kg/sec'
+        call InputReadAndConvertUnits(input,this%max_mass_rate, &
+                                      internal_units, &
+                                      'SOURCE_SINK_SANDBOX,PRESSURE,' // &
+                                      trim(word),option)
       case('PRESSURE')
         internal_units = 'Pa'
-        do i = 1, option%nphase
-          word = ''
-          select case(i)
-            case(ONE_INTEGER)
-              word = 'Liquid Phase Pressure'
-            case(TWO_INTEGER)
-              word = 'Gas Phase Pressure'
-            case default
-              write(word,*) i
-              option%io_buffer = 'Unknown Phase' // StringWrite(i) // &
-                                 ' in SrcSinkPressureRead.'
-              call PrintErrMsg(option)
-          end select
-          call InputReadDouble(input,option,this%pressure(i))
-          call InputErrorMsg(input,option,word,'SOURCE_SINK_SANDBOX,PRESSURE,')
-          call InputReadAndConvertUnits(input,this%pressure(i),internal_units, &
-                                  'SOURCE_SINK_SANDBOX,PRESSURE,'//trim(word), &
-                                        option)
-        enddo
+        call InputReadDouble(input,option,this%pressure)
+        call InputErrorMsg(input,option,word,'SOURCE_SINK_SANDBOX,PRESSURE,')
+        call InputReadAndConvertUnits(input,this%pressure,internal_units, &
+                                'SOURCE_SINK_SANDBOX,PRESSURE,'//trim(word), &
+                                      option)
       case default
         call InputKeywordUnrecognized(input,word, &
                                       'SRCSINK_SANDBOX,PRESSURE',option)
     end select
   enddo
   call InputPopBlock(input,option)
+
+  if (.not.Initialized(this%iphase)) then
+    option%io_buffer = 'PHASE LIQUID/GAS must be assigned in a PRESSURE &
+      &SOURCE_SINK_SANDBOX.'
+    call PrintErrMsg(option)
+  endif
+  if (.not.Initialized(this%max_mass_rate)) then
+    option%io_buffer = 'A MAXIMUM_MASS_RATE must be assigned in a PRESSURE &
+      &SOURCE_SINK_SANDBOX.'
+    call PrintErrMsg(option)
+  endif
+  if (.not.Initialized(this%pressure)) then
+    option%io_buffer = 'A PRESSURE must be assigned for each phase in a &
+      &PRESSURE SOURCE_SINK_SANDBOX.'
+    call PrintErrMsg(option)
+  endif
 
 end subroutine PressureRead
 
@@ -159,6 +187,7 @@ subroutine PressureSrcSink(this,Residual,Jacobian,compute_derivative, &
   use Reaction_Aux_module
   use Material_Aux_module
   use String_module
+  use Utility_module
 
   implicit none
 
@@ -170,19 +199,24 @@ subroutine PressureSrcSink(this,Residual,Jacobian,compute_derivative, &
   type(material_auxvar_type) :: material_auxvar
   PetscReal :: aux_real(:)
 
-  PetscInt :: iphase
-  PetscReal :: k
+  PetscReal :: max_rate_kmol
+  PetscReal :: scale, derivative
+  PetscReal :: xmin, xmax
 
-  k = this%rate_constant
-  do iphase = 1, option%nphase
-    Residual(iphase) = -k*(this%pressure(iphase) - aux_real(iphase))
-  enddo
-  print *, 'pumping rate: ' // &
-    StringWrite(Residual(1)/aux_real(SS_PRES_LIQUID_DENSITY_OFFSET)*3600.)
+  xmin = this%pressure-this%pressure_span
+  xmax = this%pressure
+
+  max_rate_kmol = this%max_mass_rate/FMWH2O
+  call Smoothstep(aux_real(this%iphase),xmin,xmax,scale,derivative)
+  scale = 1.d0-scale
+  derivative = -1.d0 * derivative
+  Residual(this%iphase) = max_rate_kmol*scale
+!  print *, aux_real(iphase), scale, derivative
+!  print *, 'pumping rate: ' // &
+!    StringWrite(Residual(1)/aux_real(SS_PRES_LIQUID_DENSITY_OFFSET)*3600.)
 
   if (compute_derivative) then
-    Jacobian(iphase,iphase) = k
-!    print *, 'Jac_ana:', k
+    Jacobian(this%iphase,this%iphase) = -max_rate_kmol*derivative
   endif
 
 end subroutine PressureSrcSink
