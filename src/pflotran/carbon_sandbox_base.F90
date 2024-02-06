4acffa5e84266c841f2d23d34b7e11708efb8100
Glenn Hammond <glenn.hammond@pnnl.gov>
2024-02-05 17:51:39 -0800

    began splitting carbon sandbox

--- src/pflotran/carbon_sandbox_base.F90
+++ src/pflotran/carbon_sandbox_base.F90
@@ -3,6 +3,7 @@ module Carbon_Sandbox_Base_class
 #include "petsc/finclude/petscsys.h"
   use petscsys
 
+  use Option_module
   use PFLOTRAN_Constants_module
   use Reaction_Equation_module
 
@@ -11,67 +12,92 @@ module Carbon_Sandbox_Base_class
   private
 
   type, public :: carbon_sandbox_base_type
-    PetscReal :: rate_constant
-    type(reaction_equation_type), pointer :: reaction_equation
-    character(len=MAXSTRINGLENGTH) :: reaction_string
-    procedure(CarbonEvaluate), pointer :: Evaluate => null()
+    type(carbon_sandbox_aux_type), pointer :: aux
+    class(carbon_sandbox_rxn_base_type), pointer :: rxn_list
     class(carbon_sandbox_base_type), pointer :: next
   contains
-    procedure, public :: ReadInput => CarbonBaseReadInput
+!    procedure, public :: ReadInput => CarbonBaseReadInput
     procedure, public :: Setup => CarbonBaseSetup
+    procedure, public :: Evaluate => CarbonBaseEvaluate
     procedure, public :: Destroy => CarbonBaseDestroy
   end type carbon_sandbox_base_type
 
-  interface
-    subroutine CarbonEvaluate(this,Residual,Jacobian,compute_derivative, &
-                          rt_auxvar,global_auxvar,material_auxvar, &
-                          reaction,option)
-      use Global_Aux_module
-      use Material_Aux_module
-      use Option_module
-      use Reaction_Aux_module
-      use Reactive_Transport_Aux_module
-      import :: carbon_sandbox_base_type
-      implicit none
-      class(carbon_sandbox_base_type) :: this
-      PetscReal :: Residual(:)
-      PetscReal :: Jacobian(:,:)
-      PetscBool :: compute_derivative
-      type(reactive_transport_auxvar_type) :: rt_auxvar
-      type(global_auxvar_type) :: global_auxvar
-      type(material_auxvar_type) :: material_auxvar
-      class(reaction_rt_type) :: reaction
-      type(option_type) :: option
-    end subroutine CarbonEvaluate
-  end interface
+  type, public :: carbon_sandbox_rxn_base_type
+    type(carbon_sandbox_aux_type), pointer :: aux
+    PetscReal :: rate_constant
+    type(reaction_equation_type), pointer :: reaction_equation
+    character(len=MAXSTRINGLENGTH) :: reaction_string
+    class(carbon_sandbox_rxn_base_type), pointer :: next
+  contains
+!    procedure, public :: Setup => CarbonRxnBaseSetup
+!    procedure, public :: Evaluate => CarbonRxnBaseEvaluate
+  end type carbon_sandbox_rxn_base_type
 
-  public :: CarbonBaseCreate
+  type, public :: carbon_sandbox_aux_type
+    type(option_type), pointer :: option
+    PetscReal :: liquid_saturation
+    PetscReal :: liquid_density
+    PetscReal :: porosity
+    PetscReal :: cell_volume
+    PetscReal, pointer :: conc(:)
+    PetscReal, pointer :: ln_conc(:)
+    PetscReal, pointer :: inhibition_conc(:)
+  end type carbon_sandbox_aux_type
+
+  public :: CarbonBaseCreate, &
+            CarbonRxnBaseCreate
 
 contains
 
 ! ************************************************************************** !
 
-function CarbonBaseCreate()
+function CarbonBaseCreate(option)
   !
   ! Allocates and initializes the carbon sandbox object
   !
   ! Author: Glenn Hammond
   ! Date: 02/02/24
   !
+  type(option_type), pointer :: option
+
   class(carbon_sandbox_base_type), pointer :: CarbonBaseCreate
 
   class(carbon_sandbox_base_type), pointer :: this
 
   allocate(this)
-  this%rate_constant = UNINITIALIZED_DOUBLE
-  nullify(this%reaction_equation)
-  this%reaction_string = ''
-  this%Evaluate => null()
+  nullify(this%aux)
+  nullify(this%rxn_list)
   nullify(this%next)
 
   CarbonBaseCreate => this
 
 end function CarbonBaseCreate
+
+! ************************************************************************** !
+
+function CarbonRxnBaseCreate(aux)
+  !
+  ! Allocates and initializes the carbon sandbox rxn object
+  !
+  ! Author: Glenn Hammond
+  ! Date: 02/05/24
+  !
+  class(carbon_sandbox_aux_type), pointer :: aux
+
+  class(carbon_sandbox_rxn_base_type), pointer :: CarbonRxnBaseCreate
+
+  class(carbon_sandbox_rxn_base_type), pointer :: this
+
+  allocate(this)
+  this%rate_constant = UNINITIALIZED_DOUBLE
+  this%reaction_string = ''
+  nullify(this%aux)
+  nullify(this%reaction_equation)
+  nullify(this%next)
+
+  CarbonRxnBaseCreate => this
+
+end function CarbonRxnBaseCreate
 
 ! ************************************************************************** !
 
@@ -86,7 +112,7 @@ subroutine CarbonBaseReadInput(this,input,option)
   use Option_module
   use String_module
 
-  class(carbon_sandbox_base_type) :: this
+  class(carbon_sandbox_rxn_base_type) :: this
   type(input_type), pointer :: input
   type(option_type) :: option
 
@@ -143,9 +169,38 @@ subroutine CarbonBaseSetup(this,reaction,option)
 
   class(carbon_sandbox_base_type) :: this
   class(reaction_rt_type) :: reaction
+  type(option_type), pointer :: option
+
+  allocate(this%aux)
+  this%aux%option => option
+  this%aux%liquid_saturation = UNINITIALIZED_DOUBLE
+  this%aux%liquid_density = UNINITIALIZED_DOUBLE
+  this%aux%porosity = UNINITIALIZED_DOUBLE
+  this%aux%cell_volume = UNINITIALIZED_DOUBLE
+  allocate(this%aux%conc(reaction%ncomp))
+  this%aux%conc = UNINITIALIZED_DOUBLE
+  allocate(this%aux%ln_conc(reaction%ncomp))
+  this%aux%ln_conc = UNINITIALIZED_DOUBLE
+  allocate(this%aux%inhibition_conc(reaction%ncomp))
+  this%aux%inhibition_conc = UNINITIALIZED_DOUBLE
+
+end subroutine CarbonBaseSetup
+
+! ************************************************************************** !
+
+subroutine CarbonBaseRxnSetup(this,reaction,option)
+  !
+  ! Configures the reaction and associated data structures
+  !
+  ! Author: Glenn Hammond
+  ! Date: 02/02/24
+  !
+  use Reaction_Aux_module
+
+  class(carbon_sandbox_rxn_base_type) :: this
+  class(reaction_rt_type) :: reaction
   type(option_type) :: option
 
-  this%Evaluate => CarbonBaseEvaluate
   this%reaction_equation => &
       ReactionEquationCreateFromString(this%reaction_string, &
                                        reaction%naqcomp, &
@@ -156,7 +211,7 @@ subroutine CarbonBaseSetup(this,reaction,option)
                                        reaction%immobile%names, &
                                        PETSC_FALSE,option)
 
-end subroutine CarbonBaseSetup
+end subroutine CarbonBaseRxnSetup
 
 ! ************************************************************************** !
 
@@ -186,7 +241,7 @@ subroutine CarbonBaseEvaluate(this,Residual,Jacobian,compute_derivative, &
   type(global_auxvar_type) :: global_auxvar
   type(material_auxvar_type) :: material_auxvar
   type(option_type) :: option
-
+#if 0
   PetscInt, pointer :: specid(:)
   PetscReal, pointer :: stoich(:)
   PetscReal :: effective_rate
@@ -230,7 +285,7 @@ subroutine CarbonBaseEvaluate(this,Residual,Jacobian,compute_derivative, &
     icomp = specid(i)
     Residual(icomp) = Residual(icomp) - stoich(i)*effective_rate
   enddo
-
+#endif
 end subroutine CarbonBaseEvaluate
 
 ! ************************************************************************** !
@@ -242,8 +297,39 @@ subroutine CarbonBaseDestroy(this)
   ! Author: Glenn Hammond
   ! Date: 02/02/24
   !
+  use Utility_module
+
   class(carbon_sandbox_base_type) :: this
 
+  call DeallocateArray(this%aux%conc)
+  call DeallocateArray(this%aux%ln_conc)
+  call DeallocateArray(this%aux%conc)
+  nullify(this%aux%option)
+  deallocate(this%aux)
+  nullify(this%aux)
+  call CarbonRxnBaseDestroy(this%rxn_list)
+
 end subroutine CarbonBaseDestroy
+
+! ************************************************************************** !
+
+recursive subroutine CarbonRxnBaseDestroy(this)
+  !
+  ! Destroys the carbon sandbox object
+  !
+  ! Author: Glenn Hammond
+  ! Date: 02/02/24
+  !
+  class(carbon_sandbox_rxn_base_type), pointer :: this
+
+  if (.not.associated(this)) return
+
+  call ReactionEquationDestroy(this%reaction_equation)
+  nullify(this%aux)
+
+  deallocate(this)
+  nullify(this)
+
+end subroutine CarbonRxnBaseDestroy
 
 end module Carbon_Sandbox_Base_class
