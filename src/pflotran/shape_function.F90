77ae28e8e9260aa5d3eb9b8780872bf1a6ac608d
Piyoosh Jaysaval <piyoosh.jaysaval01@gmail.com>
2024-04-15 14:23:02 -0500

    Fixing shape functions for pyramid elements assuming 1st node at (-1,-1,0), 2nd at (1,-1,0), 3rd at (1,1,0), 4th at (-1,1,0), and 5th at (0,0,1) in the natural coordinate.

--- src/pflotran/shape_function.F90
+++ src/pflotran/shape_function.F90
@@ -199,7 +199,7 @@ subroutine ShapeFunctionCalculate(shapefunction)
       N(1) = 0.5d0 * (1.d0 - zeta(1) - zeta(2)) * (1 - zeta(3))
       N(2) = 0.5d0 * zeta(1) * (1 - zeta(3))
       N(3) = 0.5d0 * zeta(2) * (1 - zeta(3))
-      ! for bottom triangle nodes at z=+1
+      ! for top triangle nodes at z=+1
       N(4) = 0.5d0 * (1.d0 - zeta(1) - zeta(2)) * (1 + zeta(3))
       N(5) = 0.5d0 * zeta(1) * (1 + zeta(3))
       N(6) = 0.5d0 * zeta(2) * (1 + zeta(3))
@@ -227,11 +227,11 @@ subroutine ShapeFunctionCalculate(shapefunction)
       DN(4,:) = (/0.d0, 0.d0, 1.d0/)
 
     case(PYR_TYPE)
-      N(1) = 1.d0/8.d0*(1.d0 - zeta(1))*(1.d0 - zeta(2))*(1.d0 - zeta(3))
-      N(2) = 1.d0/8.d0*(1.d0 + zeta(1))*(1.d0 - zeta(2))*(1.d0 - zeta(3))
-      N(3) = 1.d0/8.d0*(1.d0 + zeta(1))*(1.d0 + zeta(2))*(1.d0 - zeta(3))
-      N(4) = 1.d0/8.d0*(1.d0 - zeta(1))*(1.d0 + zeta(2))*(1.d0 - zeta(3))
-      N(5) = 1.d0/2.d0*(1.d0 + zeta(3))
+      N(1) = 0.25d0 * (1.d0 - zeta(1)) * (1.d0 - zeta(2)) * (1.d0 - zeta(3))
+      N(2) = 0.25d0 * (1.d0 + zeta(1)) * (1.d0 - zeta(2)) * (1.d0 - zeta(3))
+      N(3) = 0.25d0 * (1.d0 + zeta(1)) * (1.d0 + zeta(2)) * (1.d0 - zeta(3))
+      N(4) = 0.25d0 * (1.d0 - zeta(1)) * (1.d0 + zeta(2)) * (1.d0 - zeta(3))
+      N(5) = zeta(3)
 
       DN(1,:) = (/1.d0/8.d0*(-1.d0)*(1.d0 - zeta(2))*(1.d0 - zeta(3)), &
                   1.d0/8.d0*(1.d0 - zeta(1))*(-1.d0)*(1.d0 - zeta(3)), &
