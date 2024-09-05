7c3ea421bedffbe0fa1434a6c2e93c1f012b8f27
Glenn Hammond <glenn.hammond@pnnl.gov>
2024-09-05 16:43:34 -0700

    improved error messaging in pre-test scripting for trailing whitespace

--- .gitlab/pre-check-pflotran.sh
+++ .gitlab/pre-check-pflotran.sh
@@ -12,7 +12,7 @@ WHITESPACE_LOG="trailing_whitespace.log"
 python3 ../python/remove_trailing_whitespace.py > $WHITESPACE_LOG 2>&1
 if [ $(grep -c "No trailing whitespaces found" "$WHITESPACE_LOG") -ne 1 ]; then
   cat $WHITESPACE_LOG
-  echo "\n----- Trailing whitespace found. -----\n"
+  echo "\n----- Trailing whitespace found. These must be removed before code modifications can be accepted. -----\n"
   echo "\n----- Pre-check tests failed. -----\n"
   PRE_CHECK_TEST_CODE=1
 else
